0ad23edd0bf49e0d790bdf48ea973967e7df7054
Henri Karhatsu <henri@karhatsu.com>
2010-08-14 15:26:21 +0300

    two contests to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
-Sport.create!(:name => "Hirvenjuoksu", :key => "RUN")
-Sport.create!(:name => "Hirvenhiihto", :key => "SKI")
+run = Sport.create!(:name => "Hirvenjuoksu", :key => "RUN")
+ski = Sport.create!(:name => "Hirvenhiihto", :key => "SKI")
+
+contest1 = run.contests.build(:name => "P-Savon hirvenjuoksukisat",
+  :location => "Tervo", :start_date => '2010-08-14')
+contest1.save!
+contest2 = ski.contests.build(:name => "P-Savon hirvenhiihtokisat",
+  :location => "Karttula", :start_date => '2010-12-13')
+contest2.save!
