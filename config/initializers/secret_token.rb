a9fb988b55ea70ad8decff7228b3ae73c8ca4cca
Henri Karhatsu <henri@karhatsu.com>
2010-08-14 12:58:39 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElkSports::Application.config.secret_token = '526255f50f5932a3cd8f2efa05b6d80c1182eb6114db58c9fff19e8309e2344ef98f2a689eba8091cf8484792310a81ff0f663aaaa5ba381a3f94ea59855aae6'
