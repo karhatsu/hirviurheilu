f4635a4c8ecc0a92e6a12f7d48716fe58dc42c5a
Henri Karhatsu <henri@karhatsu.com>
2010-08-14 14:30:50 +0300

    simple home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ElkSports::Application.routes.draw do
+  resource :home
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -46,13 +48,5 @@ ElkSports::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  root :to => "home#show"
 end
