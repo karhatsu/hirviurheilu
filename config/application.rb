c3e2c7f16fa123cef6a91e7e63efd8207ac1ad5c
Henri Karhatsu <henri@karhatsu.com>
2010-08-22 18:08:35 +0300

    filter parameters in rails 3 way

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module ElkSports
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
 
     config.generators do |g|
       g.test_framework :rspec
