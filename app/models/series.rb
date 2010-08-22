01599bb4008aff2bd4efe1c205c3e3b534e05c02
Henri Karhatsu <henri@karhatsu.com>
2010-08-22 21:59:50 +0300

    shot points to sorting

--- app/models/series.rb
+++ app/models/series.rb
@@ -18,8 +18,8 @@ class Series < ActiveRecord::Base
 
   def ordered_competitors
     competitors.sort do |a, b|
-      [a.no_result_reason.to_s, b.points.to_i, b.points!.to_i] <=>
-        [b.no_result_reason.to_s, a.points.to_i, a.points!.to_i]
+      [a.no_result_reason.to_s, b.points.to_i, b.shot_points.to_i, b.points!.to_i] <=>
+        [b.no_result_reason.to_s, a.points.to_i, a.shot_points.to_i, a.points!.to_i]
     end
   end
 
