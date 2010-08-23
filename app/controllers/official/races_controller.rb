150ba884b993b37dd24b00325bc96a4cf78ec0b8
Henri Karhatsu <henri@karhatsu.com>
2010-08-23 09:50:51 +0300

    race official check to official/races controller

--- app/controllers/official/races_controller.rb
+++ app/controllers/official/races_controller.rb
@@ -1,2 +1,8 @@
 class Official::RacesController < Official::OfficialController
+  before_filter :check_race_rights
+
+  private
+  def check_race_rights
+    check_race(Race.find(params[:id]))
+  end
 end
