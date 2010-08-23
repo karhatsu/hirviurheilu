8a9af2496d99552e62fa8cfacb4439a690418a97
Henri Karhatsu <henri@karhatsu.com>
2010-08-23 10:39:05 +0300

    race form

--- app/controllers/official/races_controller.rb
+++ app/controllers/official/races_controller.rb
@@ -1,6 +1,19 @@
 class Official::RacesController < Official::OfficialController
   before_filter :check_race_rights
 
+  def edit
+    @race = Race.find(params[:id])
+  end
+
+  def update
+    @race = Race.find(params[:id])
+    if @race.update_attributes(params[:race])
+      redirect_to official_root_path
+    else
+      render :edit
+    end
+  end
+
   private
   def check_race_rights
     check_race(Race.find(params[:id]))
