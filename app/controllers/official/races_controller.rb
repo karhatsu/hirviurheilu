77b1502930e4d4e10546a4f4812651f01109c261
Henri Karhatsu <henri@karhatsu.com>
2010-08-23 10:49:00 +0300

    add race

--- app/controllers/official/races_controller.rb
+++ app/controllers/official/races_controller.rb
@@ -1,5 +1,19 @@
 class Official::RacesController < Official::OfficialController
-  before_filter :check_race_rights
+  before_filter :check_race_rights, :except => [:new, :create]
+
+  def new
+    @race = Race.new
+  end
+
+  def create
+    @race = Race.new(params[:race])
+    if @race.save
+      current_user.races << @race
+      redirect_to official_root_path
+    else
+      render :new
+    end
+  end
 
   def edit
     @race = Race.find(params[:id])
