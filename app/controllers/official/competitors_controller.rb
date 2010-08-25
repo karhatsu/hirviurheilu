620c01ce96912f1f4be8a1e40426e58886d078b7
Henri Karhatsu <henri@karhatsu.com>
2010-08-25 10:04:53 +0300

    competitor form time parameter problems fixed

--- app/controllers/official/competitors_controller.rb
+++ app/controllers/official/competitors_controller.rb
@@ -1,5 +1,6 @@
 class Official::CompetitorsController < Official::OfficialController
   before_filter :check_competitor_rights
+  before_filter :handle_time_parameters, :only => :update
 
   def index
     @series = Series.find(params[:series_id])
@@ -32,9 +33,7 @@ class Official::CompetitorsController < Official::OfficialController
 
   def update
     @competitor = Competitor.find(params[:id])
-    @competitor.attributes = params[:competitor]
-    @competitor.arrival_time = nil if params[:clear_arrival_time]
-    if @competitor.save
+    if @competitor.update_attributes(params[:competitor])
       if params[:next]
         redirect_to edit_official_series_competitor_path(@competitor.series,
           @competitor.next_competitor)
@@ -63,4 +62,43 @@ class Official::CompetitorsController < Official::OfficialController
     series = Series.find(params[:series_id])
     check_race(series.race)
   end
+
+  def handle_time_parameters
+    handle_time_parameter "start_time"
+    handle_time_parameter "arrival_time"
+  end
+
+  # f.time_text_field creates hidden date fields and visible text fields.
+  # When time is entered the first time, the params sent contain an empty date
+  # and non-empty time. This throws an error in assigning multiparameter values.
+  # For that reason we must define also date parameters before assigning form
+  # parameters to the company object.
+  # When the time fields are cleared, we must do the opposite: we must reset
+  # the date fields since otherwise the time after update would be 0:00:00.
+  def handle_time_parameter(time_name)
+    if time_cleared?(params[:competitor], time_name)
+      reset_date_parameters(params[:competitor], time_name)
+    else
+      set_default_date_parameters(params[:competitor], time_name)
+    end
+  end
+
+  def time_cleared?(competitor_params, time_name)
+    competitor_params["#{time_name}(4i)"] == '' and
+      competitor_params["#{time_name}(5i)"] == '' and
+      competitor_params["#{time_name}(6i)"] == ''
+  end
+
+  def reset_date_parameters(competitor_params, time_name)
+    competitor_params["#{time_name}(1i)"] = ""
+    competitor_params["#{time_name}(2i)"] = ""
+    competitor_params["#{time_name}(3i)"] = ""
+  end
+
+  def set_default_date_parameters(competitor_params, time_name)
+    competitor_params["#{time_name}(1i)"] = "2000"
+    competitor_params["#{time_name}(2i)"] = "1"
+    competitor_params["#{time_name}(3i)"] = "1"
+  end
+
 end
