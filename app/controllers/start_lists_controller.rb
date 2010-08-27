f8ce6dd74c7599b98470fb2381580d018cfd0df2
Henri Karhatsu <henri@karhatsu.com>
2010-08-27 08:11:36 +0300

    start list as pdf

--- app/controllers/start_lists_controller.rb
+++ app/controllers/start_lists_controller.rb
@@ -1,5 +1,11 @@
 class StartListsController < ApplicationController
   def show
     @series = Series.find(params[:series_id])
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render :pdf => "#{@series.name}-lahtolista", :layout => true
+      end
+    end
   end
 end
