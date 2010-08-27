708031847a8181ec3b9141f343bd206ec610fd43
Henri Karhatsu <henri@karhatsu.com>
2010-08-27 11:55:34 +0300

    best time method to sub series

--- app/models/sub_series.rb
+++ app/models/sub_series.rb
@@ -1,6 +1,14 @@
 class SubSeries < ActiveRecord::Base
   belongs_to :series
+  has_many :competitors
 
   validates :series, :presence => true
   validates :name, :presence => true
+
+  def best_time_in_seconds
+    if @seconds_cache
+      return @seconds_cache
+    end
+    @seconds_cache = Series.best_time_in_seconds(competitors)
+  end
 end
--- spec/models/sub_series_spec.rb
+++ spec/models/sub_series_spec.rb
@@ -14,4 +14,27 @@ describe SubSeries do
       Factory.build(:sub_series, :series => nil).should have(1).errors_on(:series)
     end
   end
+
+  describe "best_time_in_seconds" do
+    before do
+      @sub_series = Factory.build(:sub_series)
+    end
+
+    it "should call static method in Series" do
+      competitors = mock(Array)
+      @sub_series.should_receive(:competitors).once.and_return(competitors)
+      Series.should_receive(:best_time_in_seconds).with(competitors).and_return(123)
+      @sub_series.best_time_in_seconds.should == 123
+    end
+
+    describe "cache" do
+      it "should calculate in the first time and get from cache in the second" do
+        competitors = mock(Array)
+        @sub_series.should_receive(:competitors).once.and_return(competitors)
+        Series.should_receive(:best_time_in_seconds).with(competitors).once.and_return(148)
+        @sub_series.best_time_in_seconds.should == 148
+        @sub_series.best_time_in_seconds.should == 148
+      end
+    end
+  end
 end
