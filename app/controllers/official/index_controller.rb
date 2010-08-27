95fa269f5fec5f2ab4589bca3a96d1706db52d86
Henri Karhatsu <henri@karhatsu.com>
2010-08-27 13:03:53 +0300

    official index shows all races for admin

--- app/controllers/official/index_controller.rb
+++ app/controllers/official/index_controller.rb
@@ -1,5 +1,9 @@
 class Official::IndexController < Official::OfficialController
   def show
-    @races = current_user.races.find(:all, :order => 'start_date DESC')
+    if current_user.admin?
+      @races = Race.find(:all, :order => 'start_date DESC')
+    else
+      @races = current_user.races.find(:all, :order => 'start_date DESC')
+    end
   end
 end
