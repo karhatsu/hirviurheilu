2d042ee751b512d487a7385c5030b878c27070b3
Henri Karhatsu <henri@karhatsu.com>
2010-08-27 08:06:29 +0300

    results as pdf

--- app/controllers/competitors_controller.rb
+++ app/controllers/competitors_controller.rb
@@ -1,6 +1,12 @@
 class CompetitorsController < ApplicationController
   def index
     @series = Series.find(params[:series_id])
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render :pdf => "#{@series.name}-tulokset", :layout => true
+      end
+    end
   end
 
   def show
