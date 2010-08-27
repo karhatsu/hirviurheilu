0934eb7401bae85b60aa6dc0d0878c2beda3b457
Henri Karhatsu <henri@karhatsu.com>
2010-08-27 12:32:03 +0300

    jquery problems fixed in application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,15 +3,15 @@
 function insert_fields(link, method, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + method, "g")
-  $(link).before(content.replace(regexp, new_id));
+  jQuery(link).before(content.replace(regexp, new_id));
 }
 
 function remove_fields(link, hide_class, removal_question) {
   if (removal_question == "" || confirm(removal_question)) {
-    var hidden_field = $(link).prev("input[type=hidden]").val("1");
+    var hidden_field = jQuery(link).prev("input[type=hidden]").val("1");
     if (hidden_field) {
       hidden_field.value = '1';
     }
-    $(link).closest("." + hide_class).hide();
+    jQuery(link).closest("." + hide_class).hide();
   }
 }
