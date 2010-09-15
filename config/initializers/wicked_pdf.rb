abdfcc031477377a9147504d231cb86257caf001
Henri Karhatsu <henri@karhatsu.com>
2010-09-15 13:28:47 +0300

    wicked_pdf in heroku

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,7 @@
+exe_path = Rails.env == 'production' ?
+  Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s : '/usr/bin/wkhtmltopdf'
 WICKED_PDF = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
-  :exe_path => '/usr/bin/wkhtmltopdf'
+  :exe_path => exe_path
 }
