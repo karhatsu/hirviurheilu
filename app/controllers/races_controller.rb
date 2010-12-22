2a13ddfb56cfc97312cffdff838f41117d1a4709
Henri Karhatsu <henri@karhatsu.com>
2010-12-22 17:11:18 +0200

    race results pdf (all series)

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -3,6 +3,12 @@ class RacesController < ApplicationController
   def show
     @race = Race.find(params[:id])
     @is_race = true
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render :pdf => "#{@race.name} - tulokset", :layout => true
+      end
+    end
   end
 
 end
