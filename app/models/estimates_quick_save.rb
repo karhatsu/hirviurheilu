e6fb91a2ae285e920fa65e344baf2ac8e6dddc93
Henri Karhatsu <henri@karhatsu.com>
2010-12-25 18:08:45 +0200

    refactored EstimatesQuickSave

--- app/models/estimates_quick_save.rb
+++ app/models/estimates_quick_save.rb
@@ -9,21 +9,46 @@ class EstimatesQuickSave
   end
 
   def save
-    unless @string.match(/^\d+:\d+:\d+$/)
-      @error = 'Syöte vääränmuotoinen'
-      return false
-    end
-    numbers = @string.split(':')
-    race = Race.find(@race_id)
-    @competitor = race.competitors.where(:number => numbers[0]).first
-    if @competitor
-      @competitor.estimate1 = numbers[1]
-      @competitor.estimate2 = numbers[2]
-      @competitor.save!
-      return true
+    if valid_string?
+      if find_competitor
+        save_result
+        return true
+      else
+        set_unknown_competitor_error
+      end
     else
-      @error = "Numerolla #{numbers[0]} ei löytynyt kilpailijaa."
-      return false
+      set_invalid_format_error
     end
+    return false
+  end
+
+  private
+  def valid_string?
+    @string.match(/^\d+:\d+:\d+$/)
+  end
+
+  def number
+    @string.split(':')[0]
+  end
+
+  def find_competitor
+    race = Race.find(@race_id)
+    return nil unless race
+    @competitor = race.competitors.where(:number => number).first
+  end
+
+  def save_result
+    numbers = @string.split(':')
+    @competitor.estimate1 = numbers[1]
+    @competitor.estimate2 = numbers[2]
+    @competitor.save!
+  end
+
+  def set_invalid_format_error
+    @error = 'Syöte vääränmuotoinen'
+  end
+
+  def set_unknown_competitor_error
+    @error = "Numerolla #{number} ei löytynyt kilpailijaa."
   end
 end
