ed9f248781497f6c8dbbfd716a5b32ed158c8b20
Henri Karhatsu <henri@karhatsu.com>
2011-01-27 23:24:48 +0200

    Mode.switch for dev env

--- app/models/mode.rb
+++ app/models/mode.rb
@@ -1,9 +1,16 @@
 class Mode
+  @@offline = OFFLINE # see application.rb
+
   def self.offline?
-    OFFLINE
+    @@offline
   end
 
   def self.online?
-    ONLINE
+    not @@offline
+  end
+
+  def self.switch
+    raise "Mode switching allowed only in dev environment" unless Rails.env == 'development'
+    @@offline = !@@offline
   end
 end
---
+++ spec/models/mode_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe Mode do
+  describe "#switch" do
+    it "initial state should be online" do
+      Mode.should be_online
+      Mode.should_not be_offline
+    end
+
+    context "when development environment" do
+      before do
+        Rails.stub!(:env).and_return('development')
+      end
+
+      it "should change mode to offline after the first switch and " +
+        "back to online after the second" do
+        Mode.switch
+        Mode.should_not be_online
+        Mode.should be_offline
+        Mode.switch
+        Mode.should be_online
+        Mode.should_not be_offline
+        Mode.switch
+        Mode.should_not be_online
+        Mode.should be_offline
+      end
+    end
+
+    context "when not development environment" do
+      before do
+        Rails.stub!(:env).and_return('production')
+      end
+
+      it "should raise an error" do
+        lambda { Mode.switch }.should raise_error
+      end
+    end
+  end
+end
+
