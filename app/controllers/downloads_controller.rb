c7133cdb191274a4aca0bd9fae4782b2085500b2
Henri Karhatsu <henri@karhatsu.com>
2011-02-23 22:54:49 +0200

    new download paths for installer and upgrader

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -2,10 +2,10 @@ class DownloadsController < ApplicationController
   before_filter :require_user
   
   def installer
-    redirect_to 'http://www.karhatsu.com/hirviurheilu/hirviurheilu.zip'
+    redirect_to 'http://www.karhatsu.com/hirviurheilu/hirviurheilu-asennus.zip'
   end
   
   def upgrader
-    redirect_to 'http://www.karhatsu.com/hirviurheilu/hirviurheilu.zip'
+    redirect_to 'http://www.karhatsu.com/hirviurheilu/hirviurheilu-paivitys.zip'
   end
 end
