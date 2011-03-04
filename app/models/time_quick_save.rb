07725f3a8cfdd437d375010e369f1734d0a9be3d
Henri Karhatsu <henri@karhatsu.com>
2011-03-04 22:55:59 +0200

    shorter time format in quick save

--- app/models/time_quick_save.rb
+++ app/models/time_quick_save.rb
@@ -1,11 +1,11 @@
 class TimeQuickSave < QuickSave
   def initialize(race_id, string)
-    super(race_id, string, /^\d+\.\d+\.\d+\.\d+$/)
+    super(race_id, string, /^\d+\.[0-2][0-9][0-5][0-9][0-5][0-9]$/)
   end
 
   private
   def set_competitor_attrs
-    numbers = @string.split('.')
-    @competitor.arrival_time = "#{numbers[1]}:#{numbers[2]}:#{numbers[3]}"
+    numbers = @string.split('.')[1]
+    @competitor.arrival_time = "#{numbers[0,2]}:#{numbers[2,2]}:#{numbers[4,2]}"
   end
 end
--- spec/models/time_quick_save_spec.rb
+++ spec/models/time_quick_save_spec.rb
@@ -13,7 +13,7 @@ describe TimeQuickSave do
   context "when string format is correct and competitor is found" do
     describe "successfull save" do
       before do
-        @qs = TimeQuickSave.new(@race.id, '10.13.12.59')
+        @qs = TimeQuickSave.new(@race.id, '10.131259')
       end
 
       describe "#save" do
@@ -43,7 +43,7 @@ describe TimeQuickSave do
     describe "save fails" do
       before do
         @c2 = Factory.create(:competitor, :series => @series, :number => 8) # no start time
-        @qs = TimeQuickSave.new(@race.id, '8.13.12.45')
+        @qs = TimeQuickSave.new(@race.id, '8.131245')
       end
 
       describe "#save" do
@@ -77,7 +77,7 @@ describe TimeQuickSave do
       series = Factory.create(:series, :race => another_race)
       Factory.create(:competitor, :series => series, :number => 8,
         :start_time => '11:00:00')
-      @qs = TimeQuickSave.new(@race.id, '8.13.12.59')
+      @qs = TimeQuickSave.new(@race.id, '8.131209')
     end
 
     describe "#save" do
@@ -103,9 +103,9 @@ describe TimeQuickSave do
     end
   end
 
-  describe "invalid string format" do
+  describe "invalid string format (1)" do
     before do
-      @qs = TimeQuickSave.new(@race.id, '10.13.12.45.11')
+      @qs = TimeQuickSave.new(@race.id, '10.1312451')
     end
 
     describe "#save" do
