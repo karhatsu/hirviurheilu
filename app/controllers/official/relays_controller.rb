f6d3357cf5f8456eb910cf02b2ca0757edbef73f
Henri Karhatsu <henri@karhatsu.com>
2011-03-10 23:22:36 +0200

    start time fixed in relay creation

--- app/controllers/official/relays_controller.rb
+++ app/controllers/official/relays_controller.rb
@@ -1,5 +1,6 @@
 class Official::RelaysController < Official::OfficialController
   before_filter :assign_race_by_race_id, :check_assigned_race, :set_relays
+  before_filter :handle_time_parameters, :only => [:create, :update]
 
   def index
   end
@@ -22,5 +23,9 @@ class Official::RelaysController < Official::OfficialController
   private
   def set_relays
     @is_relays = true
+  end
+
+  def handle_time_parameters
+    handle_time_parameter params[:relay], "start_time"
   end
 end
