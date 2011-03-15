1e24a93c7f5cd04820238bc3d715ddb22323d59f
Henri Karhatsu <henri@karhatsu.com>
2011-03-15 10:38:52 +0200

    initial version of CsvImport

---
+++ app/models/csv_import.rb
@@ -0,0 +1,50 @@
+require 'csv'
+
+class CsvImport
+  FIRST_NAME_COLUMN = 0
+  LAST_NAME_COLUMN = 1
+  CLUB_COLUMN = 2
+  SERIES_COLUMN = 3
+  
+  def initialize(race, file_path)
+    @race = race
+    @data = CSV.read(file_path)
+    @series = {}
+    @clubs = []
+    @competitors = []
+    @errors = []
+    validate_data
+  end
+  
+  def save
+    if errors.empty?
+      @competitors.each do |comp|
+        comp.club = Club.where(:race_id => @race, :name => comp.club_name).first
+        comp.club = Club.create!(:race => @race, :name => comp.club_name) unless comp.club
+        comp.save!
+      end
+      true
+    else
+      false
+    end
+  end
+  
+  def errors
+    @errors
+  end
+  
+  private
+  def validate_data
+    @data.each do |row|
+      if row.length != 4
+        @errors << 'Tiedoston rakenne virheellinen'
+        return
+      end
+      series_name = row[SERIES_COLUMN]
+      @series[series_name] = @race.series.find_by_name(series_name)
+      @competitors << Competitor.new(:series => @series[series_name],
+        :first_name => row[FIRST_NAME_COLUMN], :last_name => row[LAST_NAME_COLUMN],
+        :club_name => row[CLUB_COLUMN]) 
+    end
+  end
+end
---
+++ spec/models/csv_import_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+describe CsvImport do
+  before do
+    @race = Factory.create(:race)
+    @race.series << Factory.build(:series, :race => @race, :name => 'N')
+    @race.series << Factory.build(:series, :race => @race, :name => 'M40')
+    @race.clubs << Factory.build(:club, :race => @race, :name => 'PS')
+  end
+  
+  context "when not correct amount of columns in each row" do
+    before do
+      @ci = CsvImport.new(@race, test_file_path('invalid_import.csv'))
+    end
+    
+    it "#save should return false" do
+      @ci.save.should be_false
+    end
+    
+    it "#errors should contain a message about invalid file format" do
+      @ci.should have(1).errors
+      @ci.errors[0].should == 'Tiedoston rakenne virheellinen'
+    end
+    
+    it "there should be no new competitors for the race" do
+      @race.should have(0).competitors
+    end
+  end
+  
+  context "when correct amount of data" do
+    before do
+      @ci = CsvImport.new(@race, test_file_path('valid_import.csv'))
+    end
+    
+    context "when all series exist" do
+      describe "#save" do
+        it "should save the defined competitors and new clubs to the database and return true" do
+          @ci.save.should be_true
+          @race.reload
+          @race.should have(2).competitors
+          c = @race.competitors.order('id')[0]
+          c.first_name.should == 'Heikki'
+          c.last_name.should == 'Räsänen'
+          c.series.name.should == 'M40'
+          c.club.name.should == 'SS'
+          c = @race.competitors.order('id')[1]
+          c.first_name.should == 'Minna'
+          c.last_name.should == 'Miettinen'
+          c.series.name.should == 'N'
+          c.club.name.should == 'PS'
+          @race.should have(2).clubs
+        end
+      end
+      
+      it "#errors should be empty" do
+        @ci.should have(0).errors
+      end
+    end
+  end
+  
+  def test_file_path(file_name)
+    File.join(Rails.root, 'spec', 'files', file_name)
+  end
+end
