3b6ce88bd91be3c42c42853e18de83342abe6ac3
Henri Karhatsu <henri@karhatsu.com>
2011-03-20 02:11:28 +0800

    team competition data copy migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110319042508) do
+ActiveRecord::Schema.define(:version => 20110319175923) do
 
   create_table "activation_keys", :force => true do |t|
     t.string   "comment",    :null => false
