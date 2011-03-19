c12af41417326ae65097cd2adfa4803cfe2f08fe
Henri Karhatsu <henri@karhatsu.com>
2011-03-19 21:03:33 +0200

    team competitions results UI using new model

--- app/controllers/team_results_controller.rb
+++
@@ -1,7 +0,0 @@
-class TeamResultsController < ApplicationController
-  before_filter :assign_race_by_race_id
-
-  def index
-    @is_team_results = true
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ ElkSports::Application.routes.draw do
   resources :prices
 
   resources :races do
-    resources :team_results
+    resources :team_competitions
     resources :relays
   end
 
