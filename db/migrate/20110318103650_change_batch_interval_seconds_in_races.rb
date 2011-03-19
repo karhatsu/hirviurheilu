20ca5b3d261f674fe6b1a09d9f29f08de1601120
Henri Karhatsu <henri@karhatsu.com>
2011-03-20 04:02:33 +0800

    race batch attribute change migrations fixed

--- db/migrate/20110318103650_change_batch_interval_seconds_in_races.rb
+++ db/migrate/20110318103650_change_batch_interval_seconds_in_races.rb
@@ -1,5 +1,10 @@
 class ChangeBatchIntervalSecondsInRaces < ActiveRecord::Migration
   def self.up
+    Race.all.each do |race|
+      unless race.batch_interval_seconds
+        race.update_attribute(:batch_interval_seconds, 180)
+      end
+    end
     change_column :races, :batch_interval_seconds, :integer, :null => false, :default => 180
   end
 
