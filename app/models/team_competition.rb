b2f477ad88ce42f5de50a7a268d7963697f1c52d
Henri Karhatsu <henri@karhatsu.com>
2011-03-19 06:42:48 +0200

    team_competition.results_for_competitors

--- app/models/team_competition.rb
+++ app/models/team_competition.rb
@@ -6,4 +6,70 @@ class TeamCompetition < ActiveRecord::Base
   validates :name, :presence => true
   validates :team_competitor_count, :numericality => { :only_integer => true,
     :greater_than => 1 }
+
+  def results_for_competitors(competitors)
+    hash = create_team_results_hash(competitors)
+    sorted_teams = sorted_teams_from_team_results_hash(hash)
+    remove_teams_without_enough_competitors(sorted_teams)
+    sorted_teams
+  end
+
+  private
+  def create_team_results_hash(competitors)
+    # { team1 => {:club => club1, :points => 0, :best_points => 0,
+    #             :best_shot_points => 0, :fastest_time => 9999999,
+    #             :competitors => []},
+    #   team2 => {...} }
+    team_results_hash = Hash.new
+    competitor_counter = Hash.new
+
+    Competitor.sort(competitors).each do |competitor|
+      break if competitor.points.nil? or competitor.unofficial
+      competitor_count = competitor_counter[competitor.club] || 0
+      if competitor_count < team_competitor_count
+        competitor_counter[competitor.club] = competitor_count + 1
+        if team_results_hash[competitor.club]
+          team_hash = team_results_hash[competitor.club]
+          update_team_hash(team_hash, competitor)
+        else
+          team_results_hash[competitor.club] = create_team_hash(competitor)
+        end
+      end
+    end
+
+    team_results_hash
+  end
+
+  def update_team_hash(team_hash, competitor)
+    team_hash[:points] += competitor.points
+    team_hash[:best_points] = competitor.points if competitor.
+      points > team_hash[:points]
+    team_hash[:best_shot_points] = competitor.shot_points if competitor.
+      points > team_hash[:best_shot_points]
+    team_hash[:fastest_time] = competitor.time_in_seconds if competitor.
+      time_in_seconds < team_hash[:fastest_time]
+    team_hash[:competitors] << competitor
+  end
+
+  def create_team_hash(competitor)
+    team_hash = Hash.new(:club => competitor.club, :points => 0, :competitors => [])
+    team_hash[:club] = competitor.club
+    team_hash[:points] = competitor.points
+    team_hash[:best_points] = competitor.points
+    team_hash[:best_shot_points] = competitor.shot_points
+    team_hash[:fastest_time] = competitor.time_in_seconds
+    team_hash[:competitors] = [competitor]
+    team_hash
+  end
+
+  def sorted_teams_from_team_results_hash(hash)
+    hash.values.sort do |a, b|
+      [b[:points], b[:best_points], b[:best_shot_points], a[:fastest_time]] <=>
+        [a[:points], a[:best_points], a[:best_shot_points], b[:fastest_time]]
+    end
+  end
+
+  def remove_teams_without_enough_competitors(sorted_teams)
+    sorted_teams.delete_if { |club| club[:competitors].length < team_competitor_count }
+  end
 end
--- spec/models/team_competition_spec.rb
+++ spec/models/team_competition_spec.rb
@@ -23,4 +23,98 @@ describe TeamCompetition do
       it { should_not allow_value(2.1).for(:team_competitor_count) }
     end
   end
+
+  describe "#results_for_competitors" do
+    before do
+      @tc = Factory.build(:team_competition, :team_competitor_count => 2)
+    end
+
+    it "should return empty array if none of the clubs have enough competitors" do
+      @club = mock_model(Club)
+      @c = mock_model(Competitor, :points => 1100, :club => @club,
+        :shot_points => 300, :time_in_seconds => 500, :unofficial => false)
+      Competitor.should_receive(:sort).with([@c]).and_return([@c])
+      @tc.results_for_competitors([@c]).should == []
+    end
+
+    context "when the clubs have enough competitors" do
+      before do
+        @club_best_total_points = mock_model(Club, :name => 'Club 1')
+        @club_best_single_points = mock_model(Club, :name => 'Club 2')
+        @club_best_single_shots = mock_model(Club, :name => 'Club 3')
+        @club_best_single_time = mock_model(Club, :name => 'Club 4')
+        @club_worst = mock_model(Club, :name => 'Club 5')
+        @club_small = mock_model(Club, :name => 'Club small')
+        @club_unofficial = mock_model(Club, :name => 'Club unofficial')
+
+        @club_best_total_points_c1 = create_competitor(@club_best_total_points, 1100)
+        @club_best_total_points_c2 = create_competitor(@club_best_total_points, 1000)
+        @club_best_total_points_c_excl = create_competitor(@club_best_total_points, 999)
+        @club_best_single_points_c1 = create_competitor(@club_best_single_points, 1050)
+        @club_best_single_points_c2 = create_competitor(@club_best_single_points, 800)
+        @club_best_single_shots_c1 = create_competitor(@club_best_single_shots, 1049)
+        @club_best_single_shots_c2 = create_competitor(@club_best_single_shots, 801,
+          :shot_points => 201)
+        @club_best_single_time_c1 = create_competitor(@club_best_single_time, 1049)
+        @club_best_single_time_c2 = create_competitor(@club_best_single_time, 801,
+          :time_in_seconds => 999)
+        @club_worst_c1 = create_competitor(@club_worst, 1049)
+        @club_worst_c2 = create_competitor(@club_worst, 801)
+
+        @club_small_c = create_competitor(@club_small, 1200)
+        @club_small_nil_points = create_competitor(@club_small, nil)
+        @club_unofficial1 = create_competitor(@club_unofficial, 1200, :unofficial => true)
+        @club_unofficial2 = create_competitor(@club_unofficial, 1200, :unofficial => true)
+
+        competitors =
+          [@club_small_c, @club_best_total_points_c1, @club_best_single_points_c1,
+            @club_best_single_shots_c1, @club_best_single_time_c1,
+            @club_worst_c1,
+            @club_best_total_points_c2, @club_best_total_points_c_excl,
+            @club_best_single_shots_c2,
+            @club_best_single_time_c2, @club_worst_c2,
+            @club_best_single_points_c2,
+            @club_unofficial1, @club_unofficial2, @club_small_nil_points]
+        Competitor.should_receive(:sort).with(competitors).and_return(competitors)
+        @results = @tc.results_for_competitors(competitors)
+      end
+
+      describe "should return an array of hashes" do
+        it "including only the clubs with enough official competitors with complete points " +
+            "so that the clubs are ordered: 1. total points " +
+            "2. best individual points 3. best individual shot points " +
+            "4. fastest individual time" do
+          @results.length.should == 5
+          @results[0][:club].should == @club_best_total_points
+          @results[1][:club].should == @club_best_single_points
+          @results[2][:club].should == @club_best_single_shots
+          @results[3][:club].should == @club_best_single_time
+          @results[4][:club].should == @club_worst
+        end
+
+        it "including total points" do
+          @results[0][:points].should == 1100 + 1000
+          @results[1][:points].should == 1050 + 800
+          @results[2][:points].should == 1049 + 801
+          @results[3][:points].should == 1049 + 801
+          @results[4][:points].should == 1049 + 801
+        end
+
+        it "including ordered competitors inside of each team" do
+          @results[0][:competitors].length.should == 2
+          @results[0][:competitors][0].should == @club_best_total_points_c1
+          @results[0][:competitors][1].should == @club_best_total_points_c2
+          @results[1][:competitors].length.should == 2
+          @results[1][:competitors][0].should == @club_best_single_points_c1
+          @results[1][:competitors][1].should == @club_best_single_points_c2
+        end
+      end
+
+      def create_competitor(club, points, options={})
+        mock_model(Competitor, {:points => points, :club => club,
+            :shot_points => 200, :time_in_seconds => 1000,
+            :unofficial => false}.merge(options))
+      end
+    end
+  end
 end
