bde50286a6768b81ec68ee6243ca2e755ca0cbaf
jkpj <jkpj@users.sourceforge.net>
2011-03-25 14:46:10 +0200

    fix to: remove extra ? from zero results when national record not set

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -65,7 +65,7 @@ module ApplicationHelper
     tag=''
     tag << 'SE' if competitor.national_record_passed?
     tag << 'SE(sivuaa)' if competitor.national_record_reached?
-    tag << '?' if competitor.national_record_reached? and not competitor.series.race.finished?
+    tag << '?' if (competitor.national_record_reached? or competitor.national_record_passed?) and not competitor.series.race.finished?
     return raw("<span class='explanation'>" + 
                '<a href="' + NATIONAL_RECORD_URL + '">' +
                tag + '</a>' + '</span>' ) if tag
