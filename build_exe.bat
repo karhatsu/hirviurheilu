1d58d1aa8a482ea1a2e6d7dba5fa91ace98c9a17
Henri Karhatsu <henri@karhatsu.com>
2011-10-23 16:05:17 +0300

    wkhtmltopdf-amd64 file not excluded from hirviurheilu.exe file

--- build_exe.bat
+++ build_exe.bat
@@ -8,8 +8,10 @@ rem - 7za.exe: http://7-zip.org/download.html (the command line version)
 SET RAILS_ENV=winoffline-prod
 CALL rake elk_sports:offline:create_db
 SET RAILS_ENV=
+CALL move bin\wkhtmltopdf-amd64 ..
 CALL cd ..
 CALL ocra elk_sports\start.rb elk_sports --no-dep-run --add-all-core --gemfile elk_sports\Gemfile --dll sqlite3.dll --output hirviurheilu.exe --icon elk_sports\public\favicon.ico -- server
 CALL 7za a hirviurheilu-asennus.zip hirviurheilu.exe hirviurheilu.sqlite3
 CALL 7za a hirviurheilu-paivitys.zip hirviurheilu.exe
+CALL move wkhtmltopdf-amd64 elk_sports\bin
 CALL cd elk_sports
