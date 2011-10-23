4c328f0164e427b2be57c1357e6f4f986eccf6b7
Henri Karhatsu <henri@karhatsu.com>
2011-10-23 14:50:49 +0300

    bin/libgcc_s_dw2-1.dll copied from winoffline branch, checked in wicked_pdf initializer

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,6 @@
 if WINDOWS_PLATFORM
   exe_path = Rails.root.join('bin', 'wkhtmltopdf.exe').to_s
+  check_path Rails.root.join('bin', 'libgcc_s_dw2-1.dll').to_s
 elsif ['staging', 'production'].include?(Rails.env)
   exe_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
 # winoffline-dev is used in Linux when testing sqlite3 migrations
@@ -8,5 +9,9 @@ elsif ['development', 'test', 'cucumber', 'winoffline-dev'].include?(Rails.env)
 else
   raise "Unknown non-Windows environment: #{Rails.env}"
 end
-raise "File #{exe_path} does not exist" unless File.exist?(exe_path)
+check_path exe_path
 WickedPdf.config[:exe_path] = exe_path
+
+def check_path(path)
+  raise "File #{exe_path} does not exist" unless File.exist?(path)
+end
