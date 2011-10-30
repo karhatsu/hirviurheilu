acf878b39917c66f2e7078442e2344fac87e9b52
Henri Karhatsu <henri@karhatsu.com>
2011-10-30 18:34:45 +0200

    own Gemfiles for Windows

--- build_exe.bat
+++ build_exe.bat
@@ -10,7 +10,7 @@ CALL rake elk_sports:offline:create_db
 SET RAILS_ENV=
 CALL move bin\wkhtmltopdf-amd64 ..
 CALL cd ..
-CALL ocra elk_sports\start.rb elk_sports --no-dep-run --add-all-core --gemfile elk_sports\Gemfile --dll sqlite3.dll --output hirviurheilu.exe --icon elk_sports\public\favicon.ico -- server
+CALL ocra elk_sports\start.rb elk_sports --no-dep-run --add-all-core --gemfile elk_sports\Gemfile-windows --dll sqlite3.dll --output hirviurheilu.exe --icon elk_sports\public\favicon.ico -- server
 CALL 7za a hirviurheilu-asennus.zip hirviurheilu.exe hirviurheilu.sqlite3
 CALL 7za a hirviurheilu-paivitys.zip hirviurheilu.exe
 CALL move wkhtmltopdf-amd64 elk_sports\bin
--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,9 @@
 require 'rubygems'
 
 # Set up gems listed in the Gemfile.
-gemfile = File.expand_path('../../Gemfile', __FILE__)
+is_windows = (RUBY_PLATFORM =~ /mswin|mingw/)
+gemfile_name = is_windows ? "Gemfile-windows" : "Gemfile"
+gemfile = File.expand_path("../../#{gemfile_name}", __FILE__)
 begin
   ENV['BUNDLE_GEMFILE'] = gemfile
   require 'bundler'
