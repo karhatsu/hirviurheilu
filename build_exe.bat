e6d66edf4ae3e4406a97cf174edb08872799273f
Henri Karhatsu <henri@karhatsu.com>
2011-11-13 18:49:04 +0200

    innosetup settings file and build changes accordingly

--- build_exe.bat
+++ build_exe.bat
@@ -10,8 +10,7 @@ CALL rake elk_sports:offline:create_db
 SET RAILS_ENV=
 CALL move bin\wkhtmltopdf-amd64 ..
 CALL cd ..
-CALL ocra elk_sports\start.rb elk_sports --no-dep-run --add-all-core --gemfile elk_sports\Gemfile-windows --dll sqlite3.dll --output hirviurheilu.exe --icon elk_sports\public\favicon.ico -- server
-CALL 7za a hirviurheilu-asennus.zip hirviurheilu.exe hirviurheilu.sqlite3
-CALL 7za a hirviurheilu-paivitys.zip hirviurheilu.exe
+CALL ocra elk_sports\start.rb elk_sports --no-dep-run --add-all-core --gemfile elk_sports\Gemfile-windows --dll sqlite3.dll --output hirviurheilu.exe --icon elk_sports\public\favicon.ico --chdir-first --no-lzma --innosetup elk_sports\hirviurheilu-offline.iss -- server
+CALL 7za a HirviurheiluOffline-asennus.zip Output\HirviurheiluOffline-asennus.exe
 CALL move wkhtmltopdf-amd64 elk_sports\bin
 CALL cd elk_sports
---
+++ hirviurheilu-offline.iss
@@ -0,0 +1,10 @@
+[Setup]
+AppName=Hirviurheilu Offline
+AppVersion=1.0
+DefaultDirName={pf}\Hirviurheilu
+DefaultGroupName=Hirviurheilu
+OutputBaseFilename=HirviurheiluOffline-asennus
+
+[Icons]
+Name: "{group}\Hirviurheilu Offline"; Filename: "{app}\hirviurheilu.exe"
+Name: "{group}\Poista Hirviurheilu Offline"; Filename: "{uninstallexe}"
