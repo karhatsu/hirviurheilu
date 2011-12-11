b047ed0d24d7bfd17c38a7a4b3d581e189ed774a
Henri Karhatsu <henri@karhatsu.com>
2011-12-11 12:09:54 +0200

    cucumber deprecation warnings fixed

--- features/step_definitions/start_list_steps.rb
+++ features/step_definitions/start_list_steps.rb
@@ -4,6 +4,6 @@ end
 
 Then /^I should see a start list row (\d+) with values:$/ do |order_number, values|
   values.rows_hash.values.each do |cell|
-    Then %{I should see "#{cell}" within "tr#comp_#{order_number}"}
+    step %{I should see "#{cell}" within "tr#comp_#{order_number}"}
   end
 end
