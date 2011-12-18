372361b994619354fe9a2736b12b0ffde1a9784f
Henri Karhatsu <henri@karhatsu.com>
2011-12-18 17:06:31 +0200

    ocra's gem problem fixed

--- build_exe.bat
+++ build_exe.bat
@@ -10,7 +10,7 @@ CALL rake elk_sports:offline:create_db
 SET RAILS_ENV=
 CALL move bin\wkhtmltopdf-amd64 ..
 CALL cd ..
-CALL ocra elk_sports\start.rb elk_sports --no-dep-run --add-all-core --gemfile elk_sports\Gemfile-windows --dll sqlite3.dll --output hirviurheilu.exe --icon elk_sports\public\favicon.ico --chdir-first --no-lzma --innosetup elk_sports\hirviurheilu-offline.iss -- server
+CALL ocra elk_sports\start.rb elk_sports --no-dep-run --add-all-core --gemfile elk_sports\Gemfile-windows --gem-all --dll sqlite3.dll --output hirviurheilu.exe --icon elk_sports\public\favicon.ico --chdir-first --no-lzma --innosetup elk_sports\hirviurheilu-offline.iss -- server
 CALL 7za a HirviurheiluOffline-asennus.zip Output\HirviurheiluOffline-asennus.exe
 CALL move wkhtmltopdf-amd64 elk_sports\bin
 CALL cd elk_sports
