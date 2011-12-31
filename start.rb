3ff89ede66b45a114da933d5f3a55f7f5ffd243c
Henri Karhatsu <henri@karhatsu.com>
2011-12-31 16:59:04 +0200

    winoffline start printing changed

--- start.rb
+++ start.rb
@@ -1,12 +1,12 @@
 Dir.chdir(File.dirname(__FILE__))
 ENV["RAILS_ENV"] = "winoffline-prod"
 
-p "TARKASTETAAN TIETOKANNAN TILA..."
+p "TARKASTETAAN TIETOKANNAN TILA... (ole hyva ja odota)"
 require File.expand_path('../config/application', __FILE__)
 require 'rake'
 ElkSports::Application.load_tasks
 task = Rake::Task["db:migrate"]
 task.invoke
 
-p "KAYNNISTETAAN HIRVIURHEILU..."
+p "KAYNNISTETAAN HIRVIURHEILU... (ole hyva ja odota)"
 load 'script/rails'
