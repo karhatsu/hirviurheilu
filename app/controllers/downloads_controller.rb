efc944a907a579a3ea6f716e3819d565710f07a4
Henri Karhatsu <henri@karhatsu.com>
2011-12-31 12:28:30 +0200

    offline installation instructions for the new installer

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -2,13 +2,9 @@ class DownloadsController < ApplicationController
   before_filter :require_user, :check_staging
   
   def installer
-    redirect_to 'http://www.karhatsu.com/hirviurheilu/hirviurheilu-asennus.zip'
+    redirect_to 'http://www.karhatsu.com/hirviurheilu/HirviurheiluOffline-asennus.exe'
   end
   
-  def upgrader
-    redirect_to 'http://www.karhatsu.com/hirviurheilu/hirviurheilu-paivitys.zip'
-  end
-
   private
   def check_staging
     redirect_to root_path if Rails.env == 'staging'
