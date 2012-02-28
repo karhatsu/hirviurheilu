ec532fbae51143a418d1713872ff80f902bd6cb0
Henri Karhatsu <henri@karhatsu.com>
2012-02-28 06:40:47 +0200

    don't destroy dev db while building offline release

--- build_exe.bat
+++ build_exe.bat
@@ -9,11 +9,12 @@ SET RAILS_ENV=winoffline-prod
 SET BUNDLE_GEMFILE=Gemfile-windows
 CALL bundle exec rake elk_sports:offline:create_db
 CALL del log\*.log
-CALL del db\offline-dev.sqlite3
+CALL move db\offline-dev.sqlite3 ..
 SET RAILS_ENV=
 CALL move bin\wkhtmltopdf-amd64 ..
 CALL cd ..
 CALL ocra elk_sports\start.rb elk_sports --no-dep-run --add-all-core --gemfile elk_sports\Gemfile-windows --gem-all --dll sqlite3.dll --dll ssleay32-1.0.0-msvcrt.dll --output hirviurheilu.exe --icon elk_sports\public\favicon.ico --chdir-first --no-lzma --innosetup elk_sports\hirviurheilu-offline.iss -- server
 CALL 7za a HirviurheiluOffline-asennus.zip Output\HirviurheiluOffline-asennus.exe
 CALL move wkhtmltopdf-amd64 elk_sports\bin
+CALL move offline-dev.sqlite3 elk_sports\db
 CALL cd elk_sports
