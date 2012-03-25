475a4ef09b353876f6802254d79e09259593af27
Henri Karhatsu <henri@karhatsu.com>
2012-03-25 13:32:07 +0300

    cup.cup_series

--- app/models/cup.rb
+++ app/models/cup.rb
@@ -3,4 +3,33 @@ class Cup < ActiveRecord::Base
   
   validates :name, :presence => true
   validates :top_competitions, :numericality => { :greater_than => 1, :only_integer => true }
+  
+  def cup_series
+    pick_series_with_same_name_in_all_races
+  end
+  
+  private
+  def pick_series_with_same_name_in_all_races
+    cup_series = []
+    race_count = races.length
+    name_to_count = collect_series_counts
+    name_to_count.each do |name, count|
+      cup_series << CupSeries.new(name) if count == race_count
+    end
+    cup_series
+  end
+
+  def collect_series_counts
+    name_to_count = Hash.new
+    races.each do |race|
+      race.series.each do |series|
+        if name_to_count.has_key?(series.name)
+          name_to_count[series.name] += 1
+        else
+          name_to_count[series.name] = 1
+        end
+      end
+    end
+    name_to_count
+  end
 end
--- spec/models/cup_spec.rb
+++ spec/models/cup_spec.rb
@@ -19,4 +19,36 @@ describe Cup do
   describe "relations" do
     it { should have_and_belong_to_many(:races) }
   end
+  
+  describe "#cup_series" do
+    it "should return an empty array when no races" do
+      Factory.build(:cup).cup_series.should == []
+    end
+    
+    it "should return an empty array when races have no series" do
+      cup = Factory.create(:cup)
+      cup.races << Factory.build(:race)
+      cup.cup_series.should == []
+    end
+    
+    it "should return series that each race has with same name and exclude others" do
+      cup = Factory.create(:cup)
+      create_race(cup, 'M', 'N', 'S17')
+      create_race(cup, 'M', 'N50', 'N')
+      create_race(cup, 'N', 'M70', 'M')
+      cup_series = cup.cup_series
+      cup_series.length.should == 2
+      cup_series[0].name.should == 'M'
+      cup_series[1].name.should == 'N'
+    end
+    
+    def create_race(cup, *series_names)
+      race = Factory.create(:race)
+      series_names.each do |series_name|
+        race.series << Factory.build(:series, :race => race, :name => series_name)
+      end
+      cup.races << race
+      race
+    end
+  end
 end
