b399029b9c3d0fccff39bc6e1d1d34c46e2c9672
Henri Karhatsu <henri@karhatsu.com>
2012-03-31 20:53:40 +0300

    speed up cup series query

--- app/controllers/cup_series_controller.rb
+++ app/controllers/cup_series_controller.rb
@@ -2,7 +2,10 @@ class CupSeriesController < ApplicationController
   def show
     @is_cup = true
     @is_cup_series = true
-    @cup = Cup.find(params[:cup_id])
+    @cup = Cup.where(:id => params[:cup_id]).
+      includes([
+        :races => [:series => [:competitors => [:shots, :age_group, :club, :series]]]
+      ]).first
     @cup_series = @cup.find_cup_series(params[:id])
   end
 end
