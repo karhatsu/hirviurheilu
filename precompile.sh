96c453d817f7fb488d0a81a3fa93e7799b9425b8
Henri Karhatsu <henri@karhatsu.com>
2012-05-02 23:43:27 +0300

    remove precompiled assets, let heroku compile them

--- precompile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-RAILS_ENV=production bundle exec rake assets:clean
-RAILS_ENV=production bundle exec rake assets:precompile
-git add public/assets
-git commit -m "assets precompiled for production"
-git log -1 --name-only
