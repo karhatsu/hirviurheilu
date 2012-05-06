03b5c2af3446d4ce67f2f7f343e397fdd5db03b6
Henri Karhatsu <henri@karhatsu.com>
2012-05-06 10:21:42 +0300

    error page is shown if unknown id for cup or cup series

--- app/controllers/cup_series_controller.rb
+++ app/controllers/cup_series_controller.rb
@@ -1,18 +1,38 @@
 class CupSeriesController < ApplicationController
   def show
+    return unless assign_cup
+    return unless assign_cup_series
     @is_cup = true
     @is_cup_series = true
-    @cup = Cup.where(:id => params[:cup_id]).
-      includes([
-        :races => [:series => [:competitors => [:shots, :age_group, :club, :series]]]
-      ]).first
-    @cup_series = @cup.cup_series.find(params[:id])
     respond_to do |format|
       format.html
       format.pdf do
         render :pdf => "#{@cup_series.name}-tulokset", :layout => true, :margin => pdf_margin,
           :header => pdf_header("#{@cup.name} - #{@cup_series.name}\n"), :footer => pdf_footer
       end
+    end
+  end
+
+  private
+  def assign_cup
+    @id = params[:cup_id]
+    @cup = Cup.where(:id => @id).
+      includes([
+        :races => [:series => [:competitors => [:shots, :age_group, :club, :series]]]
+      ]).first
+    return true if @cup
+    render 'errors/cup_not_found'
+    false
+  end
+  
+  def assign_cup_series
+    @id = params[:id]
+    begin
+      @cup_series = @cup.cup_series.find(@id)
+      return true
+    rescue ActiveRecord::RecordNotFound
+      render 'errors/cup_series_not_found'
+      return false
     end
   end
 end
