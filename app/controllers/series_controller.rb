2dce95721f62e92d15d3af96c63fd06abb741d61
Henri Karhatsu <henri@karhatsu.com>
2012-09-30 09:54:58 +0300

    html request for series controller is redirected to the competitors controller

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -3,6 +3,7 @@ class SeriesController < ApplicationController
     assign_series_by_id
     return unless @series
     respond_to do |format|
+      format.html { redirect_to(series_competitors_path(params[:id])) }
       format.json { render :json => @series.to_json(:methods => [:next_start_number, :next_start_time]) }
     end
   end
