45df66f670f579f20a8853eeafd723f31fb9e17f
Henri Karhatsu <henri@karhatsu.com>
2012-09-30 09:54:08 +0300

    series_controller can handle unknown series id case

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,6 +1,7 @@
 class SeriesController < ApplicationController
   def show
-    @series = Series.find(params[:id])
+    assign_series_by_id
+    return unless @series
     respond_to do |format|
       format.json { render :json => @series.to_json(:methods => [:next_start_number, :next_start_time]) }
     end
