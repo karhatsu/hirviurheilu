c9da2b037164cc88f98fbdf1a9ecb7ad7e5c0024
Henri Karhatsu <henri@karhatsu.com>
2013-01-01 12:03:42 +0200

    Scenario: Show results when teams are formed based on team name

--- features/step_definitions/team_competition_steps.rb
+++ features/step_definitions/team_competition_steps.rb
@@ -3,6 +3,11 @@ Given /^the race has a team competition "([^"]*)" with (\d+) competitors \/ team
     :team_competitor_count => count)
 end
 
+Given /^the race has a team name based team competition "([^"]*)" with (\d+) competitors \/ team$/ do |name, count|
+  @tc = FactoryGirl.create(:team_competition, :race => @race, :name => name,
+    :team_competitor_count => count, :use_team_name => true)
+end
+
 Given /^the team competition contains the series "([^"]*)"$/ do |series_name|
   @tc.series << @race.series.find_by_name(series_name)
 end
