0d7ed787d4cc8166d67d552a7a762b5aebb4989a
Henri Karhatsu <henri@karhatsu.com>
2013-01-06 17:33:48 +0200

    when editing competitor, main series is shown in age group menu

--- app/controllers/official/age_groups_controller.rb
+++ app/controllers/official/age_groups_controller.rb
@@ -2,8 +2,10 @@ class Official::AgeGroupsController < Official::OfficialController
   before_filter :assign_series_by_series_id
   
   def index
+    age_groups = @series.age_groups
+    age_groups.unshift(AgeGroup.new(:id => nil, :name => @series.name)) unless age_groups.empty?
     respond_to do |format|
-      format.json { render :json => @series.age_groups }
+      format.json { render :json => age_groups }
     end
   end
 end
