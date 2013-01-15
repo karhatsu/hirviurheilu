c425a9e68563675d3f022de011dd7a9eec352904
Henri Karhatsu <henri@karhatsu.com>
2013-01-15 18:12:02 +0200

    changes for continuous integration

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -5,14 +5,18 @@ end
 if WINDOWS_PLATFORM
   exe_path = Rails.root.join('bin', 'wkhtmltopdf.exe').to_s
   check_path Rails.root.join('bin', 'libgcc_s_dw2-1.dll').to_s
+elsif Rails.env.test?
+  exe_path = nil
 elsif ['staging', 'production'].include?(Rails.env)
   exe_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
 # winoffline-dev is used in Linux when testing sqlite3 migrations
-elsif ['development', 'test', 'cucumber', 'winoffline-dev'].include?(Rails.env)
+elsif ['development', 'winoffline-dev'].include?(Rails.env)
   exe_path = Rails.root.join('bin', 'wkhtmltopdf-i386').to_s
 else
   raise "Unknown non-Windows environment: #{Rails.env}"
 end
 
-check_path exe_path
-WickedPdf.config[:exe_path] = exe_path
+if exe_path
+  check_path exe_path
+  WickedPdf.config[:exe_path] = exe_path
+end
