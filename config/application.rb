d736e68ef5bd7bf9255ee26c3788f3719fab7961
Henri Karhatsu <henri@karhatsu.com>
2013-02-23 17:55:54 +0200

    i18n files reorganized

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,7 @@ module ElkSports
     config.time_zone = 'Helsinki'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :fi
 
     # JavaScript files you want as :defaults (application.js is always included).
