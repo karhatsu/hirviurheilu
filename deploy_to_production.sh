ca5b7f33ecced5e5e9b54a2531115b2b5dda0bb5
Henri Karhatsu <henri@karhatsu.com>
2014-02-16 09:41:53 +0100

    smoke tests to deploy scripts

--- deploy_to_production.sh
+++ deploy_to_production.sh
@@ -2,3 +2,4 @@
 git push production master
 heroku run rake db:migrate --app hirvi
 heroku restart --app hirvi
+ruby test/smoke/smoketests.rb http://www.hirviurheilu.com
--- deploy_to_staging.sh
+++ deploy_to_staging.sh
@@ -2,3 +2,4 @@
 git push staging master
 heroku run rake db:migrate --app hirvitesti
 heroku restart --app hirvitesti
+ruby test/smoke/smoketests.rb http://testi.hirviurheilu.com
