3cd272b03a5a652c651efbce4529db8aa3bbf0dc
Henri Karhatsu <henri@karhatsu.com>
2014-02-18 14:11:10 +0200

    correct order of js files for turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,5 +2,8 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require jquery-ui
+//= require jquery-utils-0.8.5-min
+//= require jquery.bxSlider.min
+//= require jquery.sexy-combo.pack
+//= require elk_sports
 //= require turbolinks
-//= require_tree .
