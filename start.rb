93cd3d68400dafebded1387162db0eae61608778
Henri Karhatsu <henri@karhatsu.com>
2014-02-20 11:59:02 +0200

    nicer printing to offline console

--- start.rb
+++ start.rb
@@ -1,14 +1,14 @@
 Dir.chdir(File.dirname(__FILE__))
 ENV["RAILS_ENV"] = "winoffline-prod"
 
-p "VALMISTELLAAN HIRVIURHEILUA... (ole hyva ja odota)"
+puts "VALMISTELLAAN HIRVIURHEILUA... (ole hyva ja odota)"
 require File.expand_path('../config/application', __FILE__)
 
-p "TARKASTETAAN TIETOKANNAN TILA... (ole hyva ja odota)"
+puts "TARKASTETAAN TIETOKANNAN TILA... (ole hyva ja odota)"
 require 'rake'
 ElkSports::Application.load_tasks
 task = Rake::Task["db:migrate"]
 task.invoke
 
-p "KAYNNISTETAAN HIRVIURHEILU... (ole hyva ja odota)"
+puts "KAYNNISTETAAN HIRVIURHEILU... (ole hyva ja odota)"
 load 'script/rails'
