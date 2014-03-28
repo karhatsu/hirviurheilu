c4d8e414e3086924ff4ce047dd53db81f781913a
Henri Karhatsu <henri@karhatsu.com>
2014-03-28 05:59:40 +0200

    filter parameters to initializer

--- config/application.rb
+++ config/application.rb
@@ -58,9 +58,6 @@ module ElkSports
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password, :password_confirmation]
-
     # Enable the asset pipeline
     config.assets.enabled = true
 
