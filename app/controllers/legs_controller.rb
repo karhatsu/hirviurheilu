fd9c639fd4dab3941ce0409265ce61d4a92a5873
Henri Karhatsu <henri@karhatsu.com>
2014-04-30 08:48:36 +0300

    mobile relay leg page

--- app/controllers/legs_controller.rb
+++ app/controllers/legs_controller.rb
@@ -1,5 +1,5 @@
 class LegsController < ApplicationController
-  before_action :set_races, :assign_relay_by_relay_id
+  before_action :set_races, :assign_relay_by_relay_id, :set_variant
 
   def show
     @is_relays = true
