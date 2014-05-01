39aab7eefb048d1c0c47448a31ff6b20ca4c3a82
Henri Karhatsu <henri@karhatsu.com>
2014-05-01 08:19:06 +0300

    run all tests in the beginning of deploy script

--- deploy_to_production.sh
+++ deploy_to_production.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+set -e
+spring rspec spec
+spring cucumber features
+spring cucumber -p js features
 git push production master
 heroku run rake db:migrate --app hirvi
 heroku restart --app hirvi
