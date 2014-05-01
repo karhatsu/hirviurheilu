8338b88a1fcafe6e7707892f7049129aa82b50a0
Henri Karhatsu <henri@karhatsu.com>
2014-05-01 07:50:06 +0300

    handle cup not found error

--- app/controllers/cups_controller.rb
+++ app/controllers/cups_controller.rb
@@ -1,9 +1,8 @@
 class CupsController < ApplicationController
-  before_action :set_variant
+  before_action :assign_cup_by_id, :set_variant
 
   def show
     @is_cup = true
     @is_cup_main = true
-    @cup = Cup.find(params[:id])
   end
 end
