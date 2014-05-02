afb8dee686a5fefbeefb01e8a6284305f13b95d5
Henri Karhatsu <henri@karhatsu.com>
2014-05-02 09:12:33 +0300

    run cucumber js scenarios with progress format during deploy

--- deploy_to_production.sh
+++ deploy_to_production.sh
@@ -4,8 +4,8 @@ if [ "$(cat STABLE_COMMIT)" != "$(git rev-list --max-count=1 HEAD)" ]
 then
   echo "Running all tests..."
   spring rspec spec
-  spring cucumber features
-  spring cucumber -p js features
+  spring cucumber --format progress features
+  spring cucumber -p js --format progress features
 else
   echo "No need to run tests."
 fi
--- deploy_to_staging.sh
+++ deploy_to_staging.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 spring rspec spec
-spring cucumber features
-spring cucumber -p js features
+spring cucumber --format progress features
+spring cucumber -p js --format progress features
 git rev-list --max-count=1 HEAD > STABLE_COMMIT
 git push staging master
 heroku run rake db:migrate --app hirvitesti
