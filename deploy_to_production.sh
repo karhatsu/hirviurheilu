dbba8f2341239b598c52adc5082b55d4d4452086
Henri Karhatsu <henri@karhatsu.com>
2014-05-02 09:10:34 +0300

    if deploy to prod right after staging, do not rerun tests

--- deploy_to_production.sh
+++ deploy_to_production.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -e
-spring rspec spec
-spring cucumber features
-spring cucumber -p js features
+if [ "$(cat STABLE_COMMIT)" != "$(git rev-list --max-count=1 HEAD)" ]
+then
+  echo "Running all tests..."
+  spring rspec spec
+  spring cucumber features
+  spring cucumber -p js features
+else
+  echo "No need to run tests."
+fi
 git push production master
 heroku run rake db:migrate --app hirvi
 heroku restart --app hirvi
--- deploy_to_staging.sh
+++ deploy_to_staging.sh
@@ -3,6 +3,7 @@ set -e
 spring rspec spec
 spring cucumber features
 spring cucumber -p js features
+git rev-list --max-count=1 HEAD > STABLE_COMMIT
 git push staging master
 heroku run rake db:migrate --app hirvitesti
 heroku restart --app hirvitesti
