b18f5ce3a79e5a95f055e23aef87df2bcead8319
Henri Karhatsu <henri@karhatsu.com>
2014-05-04 10:58:32 +0300

    do not run tests if all passed for the latest commit already

--- deploy_to_production.sh
+++ deploy_to_production.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -e
-if [ "$(cat STABLE_COMMIT)" != "$(git rev-list --max-count=1 HEAD)" ]
-then
-  echo "Running all tests..."
-  ./run_all_tests.sh
-else
-  echo "No need to run tests."
-fi
+./run_all_tests.sh
 git push production master
 heroku run rake db:migrate --app hirvi
 heroku restart --app hirvi
--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
-spring rspec spec
-spring cucumber --format progress features
-spring cucumber -p js --format progress features
-git rev-list --max-count=1 HEAD > STABLE_COMMIT
+if [ "$(cat STABLE_COMMIT)" != "$(git rev-list --max-count=1 HEAD)" ]
+then
+  echo "Running all tests..."
+  spring rspec spec
+  spring cucumber --format progress features
+  spring cucumber -p js --format progress features
+  git rev-list --max-count=1 HEAD > STABLE_COMMIT
+else
+  echo "No need to run tests."
+fi
