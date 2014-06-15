c2bbedc146a2e705f9bc9e340f594cf4de1c874a
Henri Karhatsu <henri@karhatsu.com>
2014-06-15 12:20:58 +0300

    modal close icon fixed using jquery-ui-themes, unused jquery images deleted

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require jquery-ui
+//= require jquery.ui.dialog
 //= require jquery-utils-0.8.5-min
 //= require jquery.bxSlider.min
 //= require elk_sports
