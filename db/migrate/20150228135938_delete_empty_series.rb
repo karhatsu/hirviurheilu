f989ef6475449668bc8dd15df7084da0fd69a0f3
Henri Karhatsu <henri@karhatsu.com>
2015-03-03 08:01:05 +0200

    cannot rely on competitors_count counter cache in delete empty series migration

--- db/migrate/20150228135938_delete_empty_series.rb
+++ db/migrate/20150228135938_delete_empty_series.rb
@@ -1,6 +1,8 @@
 class DeleteEmptySeries < ActiveRecord::Migration
   def up
-    Series.joins(:race).where('competitors_count=? and races.finished=?', 0, true).destroy_all
+    Series.joins(:race).where('races.finished=?', true).find_each do |series|
+      series.destroy if series.competitors.count == 0
+    end
   end
 
   def down
