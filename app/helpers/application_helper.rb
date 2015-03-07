dc3ea80c83fbd0b8a8c6c41491a984453d5112cf
Henri Karhatsu <henri@karhatsu.com>
2015-03-07 08:14:28 +0200

    ResultFormatHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,82 +28,6 @@ module ApplicationHelper
     raw(html)
   end
 
-  def points_print(competitor, all_competitors=false)
-    if competitor.no_result_reason
-      return no_result_reason_print(competitor.no_result_reason)
-    end
-    points = competitor.points(all_competitors)
-    return points.to_s unless points.nil?
-    partial_points = competitor.points!(all_competitors)
-    return "(#{partial_points})" unless partial_points.nil?
-    "-"
-  end
-
-  def national_record(competitor, raw=false)
-    if competitor.national_record_passed?
-      tag = 'SE'
-    elsif competitor.national_record_reached?
-      tag = 'SE(sivuaa)'
-    else
-      return ''
-    end
-    tag << '?' unless competitor.series.race.finished?
-    return tag if raw
-    return raw("<span class='explanation'>" +
-               '<a href="' + NATIONAL_RECORD_URL + '">' +
-               tag + '</a>' + '</span>' )
-    ''
-  end
-
-  def no_result_reason_print(no_result_reason, scope='competitor')
-    raw("<span class='explanation' " +
-      "title='#{t(scope + '.' + no_result_reason)}'>" +
-      "#{no_result_reason}</span>")
-  end
-
-  def time_points(competitor, with_time=false, all_competitors=false)
-    return '' if competitor.no_result_reason
-    return 300 if competitor.series.time_points_type == Series::TIME_POINTS_TYPE_ALL_300
-    return '-' if competitor.time_in_seconds.nil?
-    points = competitor.time_points(all_competitors)
-    html = (points == 300 ? "<span class='series_best_time'>" : '')
-    html << points.to_s
-    html << " (#{time_from_seconds(competitor.time_in_seconds)})" if with_time
-    html << (points == 300 ? "</span>" : '')
-    raw(html)
-  end
-
-  def relay_time_adjustment(adjustment)
-    return '' unless adjustment
-    return '' if adjustment == 0
-    html = '(<span class=\'adjustment\' title="Aika sisältää korjausta '
-    html << time_from_seconds(adjustment, true)
-    html << '">'
-    html << time_from_seconds(adjustment, true)
-    html << '</span>)'
-    raw(html)
-  end
-
-  def shot_points(competitor, shots_total=false)
-    return '' if competitor.no_result_reason
-    return "-" if competitor.shots_sum.nil?
-    points = competitor.shot_points.to_s
-    points << " (#{competitor.shots_sum})" if shots_total
-    points
-  end
-
-  def shots_list(competitor)
-    return "-" if competitor.shots_sum.nil?
-    return competitor.shots_total_input unless competitor.shots_total_input.nil?
-    shot_values = competitor.shot_values
-    list = ""
-    shot_values.each_with_index do |v, i|
-      list << "," if i > 0
-      list << "#{v.to_i}"
-    end
-    list
-  end
-
   def full_name(competitor, first_name_first=false)
     if first_name_first
       "#{competitor.first_name} #{competitor.last_name}"
@@ -266,23 +190,6 @@ module ApplicationHelper
   def link_with_protocol(link)
     return link if link[0, 7] == 'http://' or link[0, 8] == 'https://'
     'http://' + link
-  end
-  
-  def comparison_time_title(competitor, all_competitors=false, always_empty=false)
-    return '' if always_empty
-    comparison_time = competitor.comparison_time_in_seconds(all_competitors)
-    return '' unless comparison_time
-    raw " title='Vertailuaika: #{time_from_seconds(comparison_time, false)}'"
-  end
-  
-  def comparison_and_own_time_title(competitor)
-    time_in_seconds = competitor.time_in_seconds
-    return '' unless time_in_seconds
-    title = " title='Aika: #{time_from_seconds(time_in_seconds)}"
-    comparison_time_in_seconds = competitor.comparison_time_in_seconds(false)
-    title << ". Vertailuaika: #{time_from_seconds(comparison_time_in_seconds)}." if comparison_time_in_seconds
-    title << "'"
-    raw title
   end
   
   def locale_for_path
---
+++ spec/helpers/result_format_helper_spec.rb
@@ -0,0 +1,294 @@
+require 'spec_helper'
+
+describe ResultFormatHelper do
+  describe '#points_print' do
+    before do
+      @all_competitors = true
+    end
+
+    it 'should print no result reason if it is defined' do
+      competitor = instance_double(Competitor, :no_result_reason => Competitor::DNS)
+      allow(competitor).to receive(:points).with(@all_competitors).and_return(145)
+      expect(helper.points_print(competitor, @all_competitors)).
+          to eq("<span class='explanation' title='Kilpailija ei osallistunut kilpailuun'>DNS</span>")
+    end
+
+    it 'should print points in case they are available' do
+      competitor = instance_double(Competitor, :no_result_reason => nil,
+                                   :series => nil)
+      expect(competitor).to receive(:points).with(@all_competitors).and_return(145)
+      expect(helper.points_print(competitor, @all_competitors)).to eq('145')
+    end
+
+    it 'should print points in brackets if only partial points are available' do
+      competitor = instance_double(Competitor, :no_result_reason => nil)
+      expect(competitor).to receive(:points).with(@all_competitors).and_return(nil)
+      expect(competitor).to receive(:points!).with(@all_competitors).and_return(100)
+      expect(helper.points_print(competitor, @all_competitors)).to eq('(100)')
+    end
+
+    it 'should print - if no points at all' do
+      competitor = instance_double(Competitor, :no_result_reason => nil,
+                                   :series => nil)
+      expect(competitor).to receive(:points).with(@all_competitors).and_return(nil)
+      expect(competitor).to receive(:points!).with(@all_competitors).and_return(nil)
+      expect(helper.points_print(competitor, @all_competitors)).to eq('-')
+    end
+  end
+
+  describe '#relay_time_adjustment' do
+    before do
+      allow(helper).to receive(:time_from_seconds).and_return('00:01')
+    end
+
+    it 'should return nothing when nil given' do
+      expect(helper.relay_time_adjustment(nil)).to eq('')
+    end
+
+    it 'should return nothing when 0 seconds given' do
+      expect(helper.relay_time_adjustment(0)).to eq('')
+    end
+
+    it 'should return the html span block when 1 second given' do
+      expect(helper.relay_time_adjustment(1)).to eq("(<span class='adjustment' title=\"Aika sisältää korjausta 00:01\">00:01</span>)")
+    end
+  end
+
+  describe '#shot_points' do
+    context 'when reason for no result' do
+      it 'should return empty string' do
+        competitor = stub_competitor 500, 88, Competitor::DNS
+        expect(helper.shot_points(competitor)).to eq('')
+      end
+    end
+
+    context 'when no shots sum' do
+      it 'should return dash' do
+        competitor = stub_competitor 150, nil
+        expect(helper.shot_points(competitor)).to eq('-')
+      end
+    end
+
+    context 'when no total shots wanted' do
+      it 'should return shot points' do
+        competitor = stub_competitor 480, 80
+        expect(helper.shot_points(competitor)).to eq('480')
+      end
+    end
+
+    context 'when total shots wanted' do
+      it 'should return shot points and sum in brackets' do
+        competitor = stub_competitor 480, 80
+        expect(helper.shot_points(competitor, true)).to eq('480 (80)')
+      end
+    end
+
+    def stub_competitor(shot_points, shots_sum, no_result_reason=nil)
+      instance_double Competitor, shot_points: shot_points, shots_sum: shots_sum, no_result_reason: no_result_reason
+    end
+  end
+
+  describe '#shots_list' do
+    it 'should return dash when no shots sum' do
+      competitor = instance_double(Competitor, :shots_sum => nil)
+      expect(helper.shots_list(competitor)).to eq('-')
+    end
+
+    it "should return input total if such is given" do
+      competitor = instance_double(Competitor, :shots_sum => 45, :shots_total_input => 45)
+      expect(helper.shots_list(competitor)).to eq(45)
+    end
+
+    it 'should return comma separated list if individual shots defined' do
+      shots = [10, 1, 9, 5, 5, nil, nil, 6, 4, 0]
+      competitor = instance_double(Competitor, :shots_sum => 50,
+                                   :shots_total_input => nil, :shot_values => shots)
+      expect(helper.shots_list(competitor)).to eq('10,1,9,5,5,0,0,6,4,0')
+    end
+  end
+
+  describe '#time_points' do
+    before do
+      @series = instance_double(Series, :time_points_type => Series::TIME_POINTS_TYPE_NORMAL)
+    end
+
+    context 'when reason for no result' do
+      it 'should return empty string' do
+        competitor = instance_double(Competitor, :series => @series, :no_result_reason => Competitor::DNS)
+        expect(helper.time_points(competitor)).to eq('')
+      end
+    end
+
+    context 'when 300 points for all competitors in this series' do
+      it 'should return 300' do
+        allow(@series).to receive(:time_points_type).and_return(Series::TIME_POINTS_TYPE_ALL_300)
+        competitor = instance_double(Competitor, :series => @series, :no_result_reason => nil)
+        expect(helper.time_points(competitor)).to eq(300)
+      end
+    end
+
+    context 'when no time' do
+      it 'should return dash' do
+        competitor = instance_double(Competitor, :time_in_seconds => nil,
+                                     :no_result_reason => nil, :series => @series)
+        expect(helper.time_points(competitor)).to eq('-')
+      end
+    end
+
+    context 'when time points and time wanted' do
+      it 'should return time points and time in brackets' do
+        all_competitors = true
+        competitor = instance_double(Competitor, :series => @series,
+                                     :time_in_seconds => 2680, :no_result_reason => nil)
+        expect(competitor).to receive(:time_points).with(all_competitors).and_return(270)
+        expect(helper).to receive(:time_from_seconds).with(2680).and_return('45:23')
+        expect(helper.time_points(competitor, true, all_competitors)).to eq('270 (45:23)')
+      end
+
+      it 'should wrap with best time span when full points' do
+        all_competitors = true
+        competitor = instance_double(Competitor, :series => @series,
+                                     :time_in_seconds => 2680, :no_result_reason => nil)
+        expect(competitor).to receive(:time_points).with(all_competitors).and_return(300)
+        expect(helper).to receive(:time_from_seconds).with(2680).and_return('45:23')
+        expect(helper.time_points(competitor, true, all_competitors)).
+            to eq("<span class='series_best_time'>300 (45:23)</span>")
+      end
+    end
+
+    context 'when time points but no time wanted' do
+      it 'should return time points' do
+        all_competitors = true
+        competitor = instance_double(Competitor, :series => @series,
+                                     :time_in_seconds => 2680, :no_result_reason => nil)
+        expect(competitor).to receive(:time_points).with(all_competitors).and_return(270)
+        expect(helper.time_points(competitor, false, all_competitors)).to eq('270')
+      end
+
+      it 'should wrap with best time span when full points' do
+        all_competitors = true
+        competitor = instance_double(Competitor, :series => @series,
+                                     :time_in_seconds => 2680, :no_result_reason => nil)
+        expect(competitor).to receive(:time_points).with(all_competitors).and_return(300)
+        expect(helper.time_points(competitor, false, all_competitors)).
+            to eq("<span class='series_best_time'>300</span>")
+      end
+    end
+  end
+
+  describe '#comparison_time_title' do
+    before do
+      @competitor = instance_double(Competitor)
+      allow(@competitor).to receive(:comparison_time_in_seconds).and_return(1545)
+    end
+
+    it 'should return empty string when empty always wanted' do
+      expect(helper.comparison_time_title(@competitor, true, true)).to eq('')
+    end
+
+    it 'should return empty string when no comparison time available' do
+      allow(@competitor).to receive(:comparison_time_in_seconds).and_return(nil)
+      expect(helper.comparison_time_title(@competitor, true, false)).to eq('')
+    end
+
+    it 'should return space and title attribute with title and comparison time when empty not wanted' do
+      expect(helper.comparison_time_title(@competitor, true, false)).to eq(" title='Vertailuaika: 25:45'")
+    end
+
+    it 'should use all_competitors parameter when getting the comparison time' do
+      allow(@competitor).to receive(:comparison_time_in_seconds).with(false).and_return(1550)
+      expect(helper.comparison_time_title(@competitor, false, false)).to eq(" title='Vertailuaika: 25:50'")
+    end
+  end
+
+  describe '#comparison_and_own_time_title' do
+    context 'when no time for competitor' do
+      it 'should return empty string' do
+        competitor = instance_double(Competitor)
+        allow(competitor).to receive(:time_in_seconds).and_return(nil)
+        expect(helper.comparison_and_own_time_title(competitor)).to eq('')
+      end
+    end
+
+    context 'when no comparison time for competitor' do
+      it 'should return space and title attribute with time title and time' do
+        competitor = instance_double(Competitor)
+        expect(competitor).to receive(:time_in_seconds).and_return(123)
+        expect(competitor).to receive(:comparison_time_in_seconds).with(false).and_return(nil)
+        expect(helper).to receive(:time_from_seconds).with(123).and_return('1:23')
+        expect(helper.comparison_and_own_time_title(competitor)).to eq(" title='Aika: 1:23'")
+      end
+    end
+
+    context 'when own and comparison time available' do
+      it 'should return space and title attribute with time title, time, comparison time title and comparison time' do
+        competitor = instance_double(Competitor)
+        expect(competitor).to receive(:time_in_seconds).and_return(123)
+        expect(competitor).to receive(:comparison_time_in_seconds).with(false).and_return(456)
+        expect(helper).to receive(:time_from_seconds).with(123).and_return('1:23')
+        expect(helper).to receive(:time_from_seconds).with(456).and_return('4:56')
+        expect(helper.comparison_and_own_time_title(competitor)).to eq(" title='Aika: 1:23. Vertailuaika: 4:56.'")
+      end
+    end
+  end
+
+  describe '#national_record' do
+    before do
+      @competitor = instance_double(Competitor)
+      @race = instance_double(Race)
+      series = instance_double(Series)
+      allow(@competitor).to receive(:series).and_return(series)
+      allow(series).to receive(:race).and_return(@race)
+    end
+
+    context 'when race finished' do
+      before do
+        allow(@race).to receive(:finished?).and_return(true)
+      end
+
+      context 'when national record passed' do
+        it 'should return SE' do
+          allow(@competitor).to receive(:national_record_passed?).and_return(true)
+          expect(helper.national_record(@competitor, true)).to eq('SE')
+        end
+      end
+
+      context 'when national record reached' do
+        it 'should return SE(sivuaa)' do
+          allow(@competitor).to receive(:national_record_passed?).and_return(false)
+          allow(@competitor).to receive(:national_record_reached?).and_return(true)
+          expect(helper.national_record(@competitor, true)).to eq('SE(sivuaa)')
+        end
+      end
+    end
+
+    context 'when race not finished' do
+      before do
+        allow(@race).to receive(:finished?).and_return(false)
+      end
+
+      context 'when national record passed' do
+        it 'should return SE?' do
+          allow(@competitor).to receive(:national_record_passed?).and_return(true)
+          expect(helper.national_record(@competitor, true)).to eq('SE?')
+        end
+      end
+
+      context 'when national record reached' do
+        it 'should return SE(sivuaa)?' do
+          allow(@competitor).to receive(:national_record_passed?).and_return(false)
+          allow(@competitor).to receive(:national_record_reached?).and_return(true)
+          expect(helper.national_record(@competitor, true)).to eq('SE(sivuaa)?')
+        end
+      end
+    end
+
+    context 'with decoration' do
+      it 'should surround text with span and link' do
+        allow(@race).to receive(:finished?).and_return(true)
+        allow(@competitor).to receive(:national_record_passed?).and_return(true)
+        expect(helper.national_record(@competitor, false)).to eq("<span class='explanation'><a href=\"" + NATIONAL_RECORD_URL + "\">SE</a></span>")
+      end
+    end
+  end
+end
