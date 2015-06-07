ea7b1be81f130e661c5c1b0b857184e43937f1a8
Henri Karhatsu <henri@karhatsu.com>
2015-06-07 07:53:13 +0300

    admin can list races without billing info

--- app/controllers/admin/races_controller.rb
+++ app/controllers/admin/races_controller.rb
@@ -2,8 +2,14 @@ class Admin::RacesController < Admin::AdminController
   before_action :set_admin_races
   
   def index
-    @past_races = Race.where('start_date<?', Date.today).includes(:users).order('start_date desc')
-    @future_races = Race.where('start_date>=?', Date.today).includes(:users).order('start_date desc')
+    @past_races = Race.where('start_date<?', Date.today)
+    @future_races = Race.where('start_date>=?', Date.today)
+    if params[:no_billing]
+      @past_races = @past_races.where('billing_info is null')
+      @future_races = @future_races.where('billing_info is null')
+    end
+    @past_races = @past_races.includes(:users).order('start_date desc')
+    @future_races = @future_races.includes(:users).order('start_date desc')
   end
   
   def show
@@ -18,7 +24,7 @@ class Admin::RacesController < Admin::AdminController
     race = Race.find(params[:id])
     race.update!(video_params)
     flash[:success] = 'Kilpailu tallennettu'
-    redirect_to admin_races_path
+    redirect_to admin_races_path(no_billing: params[:no_billing])
   end
   
   def destroy
