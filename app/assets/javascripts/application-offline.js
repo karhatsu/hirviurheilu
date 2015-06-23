25d7e06bd4e0197ebc3d4e5b955c9d19e0d86390
Henri Karhatsu <henri@karhatsu.com>
2015-06-23 23:32:03 +0300

    no turbolinks for offline (problems with Windows)

---
+++ app/assets/javascripts/application-offline.js
@@ -0,0 +1,7 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery.ui.dialog
+//= require jquery-utils-0.8.5-min
+//= require jquery.bxSlider.min
+//= require jquery.filtertable.min
+//= require elk_sports
