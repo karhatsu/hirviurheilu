75827dce563947516cd641475ba53df0ae145edd
Henri Karhatsu <henri@karhatsu.com>
2015-06-28 12:01:36 +0300

    application-offline.js taken into use in actual offline

--- build_exe.bat
+++ build_exe.bat
@@ -16,7 +16,7 @@ CALL move db\offline-dev.sqlite3 ..
 SET RAILS_ENV=
 CALL move bin\wkhtmltopdf-amd64 ..
 CALL copy public\assets\offline\application.css public\assets
-CALL copy public\assets\offline\application.js public\assets
+CALL copy public\assets\offline\application-offline.js public\assets
 CALL cd ..
 CALL ocra elk_sports\start.rb elk_sports --no-dep-run --add-all-core --gemfile elk_sports\Gemfile-windows --gem-all --dll sqlite3.dll --dll ssleay32.dll --dll libyaml-0-2.dll --output hirviurheilu.exe --icon elk_sports\public\favicon.ico --chdir-first --no-lzma --innosetup elk_sports\hirviurheilu-offline.iss -- server || goto :ocraerror
 CALL 7za a HirviurheiluOffline-asennus.zip Output\HirviurheiluOffline-asennus.exe
@@ -24,5 +24,5 @@ CALL 7za a HirviurheiluOffline-asennus.zip Output\HirviurheiluOffline-asennus.ex
 CALL move wkhtmltopdf-amd64 elk_sports\bin
 CALL move offline-dev.sqlite3 elk_sports\db
 CALL del elk_sports\public\assets\application.css
-CALL del elk_sports\public\assets\application.js
+CALL del elk_sports\public\assets\application-offline.js
 CALL cd elk_sports
