9eceea650dd3aca59bcd5b94b0f8a83184bb35ab
Henri Karhatsu <henri@karhatsu.com>
2015-07-29 08:41:08 +0300

    offline installer file name contains version (prevents possible caching issue)

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,9 +1,11 @@
 class DownloadsController < ApplicationController
+  OFFLINE_VERSION = '1.7.0'
+
   before_action :require_user, :check_staging
   
   def installer
     OfflineDownloadMailer.download(current_user).deliver_now
-    redirect_to 'http://www.karhatsu.com/hirviurheilu/HirviurheiluOffline-asennus.exe'
+    redirect_to "http://www.karhatsu.com/hirviurheilu/HirviurheiluOffline-asennus-#{OFFLINE_VERSION}.exe"
   end
   
   private
