b50b8e6b35db8d5bec097694312850551a9dc1e4
Henri Karhatsu <henri@karhatsu.com>
2015-10-14 08:52:00 +0300

    new heroku app names to deploy scripts

--- deploy_to_production.sh
+++ deploy_to_production.sh
@@ -2,6 +2,6 @@
 set -e
 ./run_all_tests.sh
 git push production master
-heroku run rake db:migrate --app hirvi
-heroku restart --app hirvi
+heroku run rake db:migrate --app hirviurheilu
+heroku restart --app hirviurheilu
 ruby test/smoke/smoketests.rb http://www.hirviurheilu.com
--- deploy_to_staging.sh
+++ deploy_to_staging.sh
@@ -2,6 +2,6 @@
 set -e
 ./run_all_tests.sh
 git push staging master
-heroku run rake db:migrate --app hirvitesti
-heroku restart --app hirvitesti
+heroku run rake db:migrate --app hirviurheilu-testi
+heroku restart --app hirviurheilu-testi
 ruby test/smoke/smoketests.rb http://testi.hirviurheilu.com
