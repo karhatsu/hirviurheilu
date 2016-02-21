12954fb998c4009c5f1ceffb5aee30848c036d75
Henri Karhatsu <henri@karhatsu.com>
2016-02-21 08:13:15 +0200

    better headers printing on error email

--- app/mailers/error_mailer.rb
+++ app/mailers/error_mailer.rb
@@ -2,7 +2,9 @@ class ErrorMailer < ApplicationMailer
   def error_mail(subject, exception, request, params, current_user)
     @exception = exception
     @path = request.fullpath
-    @request = request
+    @headers = request.headers.select { |key, value| key.match(/^[A-Z_]+$/) && key.match(/^HTTP.*/) }
+                   .inject({}) { |hash, (key, value)| hash[key['HTTP_'.length..-1]] = value; hash }
+    @env = request.headers.select { |key, value| key.match(/^[A-Z_]+$/) && !key.match(/^HTTP.*/) }
     @params = params
     @user = current_user
     mail :to => ADMIN_EMAIL, :from => NOREPLY_ADDRESS,
