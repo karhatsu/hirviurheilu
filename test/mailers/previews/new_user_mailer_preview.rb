e53d0fa665b5826dd536053a2f2caa9778c5e360
Henri Karhatsu <henri@karhatsu.com>
2016-07-03 14:14:01 +0200

    add preview for new user mailer

---
+++ test/mailers/previews/new_user_mailer_preview.rb
@@ -0,0 +1,9 @@
+class NewUserMailerPreview < ActionMailer::Preview
+  def new_user
+    NewUserMailer.new_user User.last
+  end
+
+  def from_admin
+    NewUserMailer.from_admin User.last, 'http://www.hirviurheilu.com/login'
+  end
+end
