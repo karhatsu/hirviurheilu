bedc769eb49971a42be97d3f235a11365a9e1265
Henri Karhatsu <henri@karhatsu.com>
2017-01-01 16:30:09 +0200

    add M40 and N40 age groups to default series

--- app/models/default_series.rb
+++ app/models/default_series.rb
@@ -21,12 +21,12 @@ class DefaultSeries
       DefaultSeries.new('S15', ['T15', 0], ['P15', 0]),
       DefaultSeries.new('S17', ['T17', 0], ['P17', 0]),
       DefaultSeries.new('S20', ['T20', 0], ['P20', 0]),
-      DefaultSeries.new('M'),
+      DefaultSeries.new('M', ['M40', 2]),
       DefaultSeries.new('M50'),
       DefaultSeries.new('M60', ['M65', 2]),
       DefaultSeries.new('M70', ['M75', 2]),
       DefaultSeries.new('M80', ['M85', 2], ['M90', 2]),
-      DefaultSeries.new('N'),
+      DefaultSeries.new('N', ['N40', 2]),
       DefaultSeries.new('N50', ['N55', 2], ['N60', 2]),
       DefaultSeries.new('N65', ['N70', 2], ['N75', 2], ['N80', 2], ['N85', 2], ['N90', 2])
     ]
