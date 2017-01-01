e204e8cb755cb0b380a56b56a84fad3da2cbe537
Henri Karhatsu <henri@karhatsu.com>
2017-01-01 11:16:31 +0200

    add validation_spec_helpers

---
+++ spec/support/validation_spec_helpers.rb
@@ -0,0 +1,27 @@
+shared_examples_for 'non-negative decimal' do |attribute|
+  it { should validate_numericality_of(attribute) }
+  it { should_not allow_value(-1).for(attribute) }
+  it { should allow_value(0.1).for(attribute) }
+  it { should allow_value(0).for(attribute) }
+end
+
+shared_examples_for 'positive decimal' do |attribute|
+  it { should validate_numericality_of(attribute) }
+  it { should_not allow_value(-1).for(attribute) }
+  it { should_not allow_value(0).for(attribute) }
+  it { should allow_value(0.1).for(attribute) }
+end
+
+shared_examples_for 'non-negative integer' do |attribute|
+  it { should validate_numericality_of(attribute) }
+  it { should_not allow_value(-1).for(attribute) }
+  it { should allow_value(0).for(attribute) }
+  it { should_not allow_value(1.1).for(attribute) }
+end
+
+shared_examples_for 'positive integer' do |attribute|
+  it { should validate_numericality_of(attribute) }
+  it { should_not allow_value(-1).for(attribute) }
+  it { should_not allow_value(0).for(attribute) }
+  it { should_not allow_value(1.1).for(attribute) }
+end
