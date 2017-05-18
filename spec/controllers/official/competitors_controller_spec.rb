cfc725cbca71d9e4ced89f0a95d1b6730866d5a6
Henri Karhatsu <henri@karhatsu.com>
2017-05-18 09:04:28 +0300

    fix official/competitors_controller_spec.rb for Rails 5

--- spec/controllers/official/competitors_controller_spec.rb
+++ spec/controllers/official/competitors_controller_spec.rb
@@ -11,7 +11,7 @@ describe Official::CompetitorsController, type: :controller do
       skip_before_action :require_user, :check_rights, :set_official
       skip_before_action :assign_series_by_series_id, :check_assigned_series
       skip_before_action :assign_race_by_race_id, :check_assigned_race
-      skip_before_action :handle_start_time, :handle_time_parameters
+      skip_before_action :handle_start_time, only: :create
       skip_before_action :set_competitors, :ensure_user_in_offline
 
       def create
