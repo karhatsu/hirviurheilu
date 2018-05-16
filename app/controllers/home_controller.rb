66cb1cbfe6f107bdb506c079d71f1d136268edc3
Henri Karhatsu <henri@karhatsu.com>
2018-05-16 08:06:23 +0300

    refactor HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,20 +4,23 @@ class HomeController < ApplicationController
 
   def show
     @is_main_page = true
-    all_cups = Cup.joins(races: :cups).includes(races: :cups)
-    all_cups = all_cups.where('races.district_id=?', params[:district_id]) unless params[:district_id].blank?
-    @today = Race.today
-    @today = @today.where(district_id: params[:district_id]) unless params[:district_id].blank?
-    @past = past_competitions(all_cups)
-    @future = Race.future
-    @future = @future.where(district_id: params[:district_id]) unless params[:district_id].blank?
-    @future = group_future_races(@future)
+    @today = competitions_today
+    @past = past_competitions
+    @future = future_competitions
     @announcements = Announcement.active.front_page
   end
 
   private
 
-  def past_competitions(all_cups)
+  def competitions_today
+    today = Race.today
+    today = today.where(district_id: params[:district_id]) unless params[:district_id].blank?
+    today
+  end
+
+  def past_competitions
+    all_cups = Cup.joins(races: :cups).includes(races: :cups)
+    all_cups = all_cups.where('races.district_id=?', params[:district_id]) unless params[:district_id].blank?
     finished_cups = all_cups.select { |cup| cup.end_date && cup.end_date < Date.today }
     finished_cups_race_ids = finished_cups.each { |cup| cup.races.map(&:id) }.flatten
 
@@ -26,5 +29,11 @@ class HomeController < ApplicationController
     past_races = past_races.where(district_id: params[:district_id]) unless params[:district_id].blank?
 
     (past_races + finished_cups).sort { |a,b| [b.end_date, a.name] <=> [a.end_date, b.name] }
+  end
+
+  def future_competitions
+    future = Race.future
+    future = future.where(district_id: params[:district_id]) unless params[:district_id].blank?
+    group_future_races(future)
   end
 end
