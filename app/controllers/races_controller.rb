fa67539bca2ea39155a1e8553eb0bea82d5328fd
Henri Karhatsu <henri@karhatsu.com>
2018-07-26 19:33:27 +0300

    add search text field to races page

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -4,9 +4,19 @@ class RacesController < ApplicationController
   before_action :set_variant
 
   def index
-    where = 'district_id=?' unless params[:district_id].blank?
-    where_params = [params[:district_id]] unless params[:district_id].blank?
-    @competitions = Race.where(where, where_params).order('start_date DESC').page(params[:page])
+    where = []
+    where_params = Hash.new
+    district_id = params[:district_id]
+    search_text = params[:search_text]
+    unless district_id.blank?
+      where << 'district_id=:district_id'
+      where_params[:district_id] = [district_id]
+    end
+    unless search_text.blank?
+      where << "(name ILIKE :search_text OR location ILIKE :search_text)"
+      where_params[:search_text] = "%#{search_text}%"
+    end
+    @competitions = Race.where(where.join(' AND '), where_params).order('start_date DESC').page(params[:page])
   end
 
   def show
