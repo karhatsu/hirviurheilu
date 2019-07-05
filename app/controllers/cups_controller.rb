cf02af7b5788e54bfacd74a8434958aea8f06272
Henri Karhatsu <henri@karhatsu.com>
2019-07-05 08:17:47 +0300

    Implement pdf for cups

--- app/controllers/cups_controller.rb
+++ app/controllers/cups_controller.rb
@@ -4,5 +4,14 @@ class CupsController < ApplicationController
   def show
     @is_cup = true
     @is_cup_main = true
+    respond_to do |format|
+      format.html
+      format.pdf do
+        @page_breaks = params[:page_breaks]
+        render :pdf => "#{@cup.name} - tulokset", :layout => true,
+               :margin => pdf_margin, :header => pdf_header("#{@cup.name} - Tuloskooste"), :footer => pdf_footer,
+               :orientation => 'Landscape', disable_smart_shrinking: true
+      end
+    end
   end
 end
