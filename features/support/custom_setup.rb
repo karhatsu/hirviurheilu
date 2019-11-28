7cce8683ac03002407c0c1a5df1b63aa1e569661
Henri Karhatsu <henri@karhatsu.com>
2019-11-28 16:27:30 +0200

    Renew cucumber setup

--- features/support/custom_setup.rb
+++ features/support/custom_setup.rb
@@ -1,9 +1,16 @@
 require 'email_spec/cucumber'
 require 'cucumber/rspec/doubles'
+require 'factory_bot_rails'
+World(FactoryBot::Syntax::Methods)
+
+Capybara.server = :webrick
+Capybara.javascript_driver = :selenium_chrome_headless
 
 def setup_database
   Role.ensure_default_roles_exist
   District.find_or_create_by! name: 'Test district', short_name: 'TD'
 end
 
-setup_database
+Before do
+  setup_database
+end
