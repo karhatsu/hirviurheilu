1f62a756ac693f9321aea5a26c74e1c8449450ff
Henri Karhatsu <henri@karhatsu.com>
2019-12-08 18:17:14 +0200

    Stop showing cups in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,16 +19,9 @@ class HomeController < ApplicationController
   end
 
   def past_competitions
-    all_cups = Cup.joins(races: :cups).includes(races: :cups)
-    all_cups = all_cups.where('races.district_id=?', params[:district_id]) unless params[:district_id].blank?
-    finished_cups = all_cups.select { |cup| cup.end_date && cup.end_date < Date.today }
-    finished_cups_race_ids = finished_cups.map { |cup| cup.races.map(&:id) }.flatten
-
     past_races = Race.past
-    past_races = past_races.where('id NOT IN(?)', finished_cups_race_ids) if finished_cups_race_ids.length > 0
     past_races = past_races.where(district_id: params[:district_id]) unless params[:district_id].blank?
-
-    (past_races + finished_cups).sort { |a,b| [b.end_date, a.name] <=> [a.end_date, b.name] }
+    past_races
   end
 
   def future_competitions
