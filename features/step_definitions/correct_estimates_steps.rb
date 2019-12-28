e98568a54e85454b6ef2d0c180458b0e0627fb20
Henri Karhatsu <henri@karhatsu.com>
2019-12-28 21:30:54 +0200

    Rename horizontal form fields css class

--- features/step_definitions/correct_estimates_steps.rb
+++ features/step_definitions/correct_estimates_steps.rb
@@ -3,7 +3,7 @@ Given /^the race has correct estimates with attributes:$/ do |fields|
 end
 
 When(/^I fill correct estimate (\d+) and (\d+) for the range (\d+)\-(\d+) in row (\d+)$/) do |distance1, distance2, min, max, index|
-  within(:xpath, "//div[@class='form__fields--horizontal'][#{index}]") do
+  within(:xpath, "//div[@class='form__horizontal-fields'][#{index}]") do
     fill_in "Lähtönumerot", with: min
     fill_in '–', with: max
     fill_in "Etäisyys 1", with: distance1
@@ -12,7 +12,7 @@ When(/^I fill correct estimate (\d+) and (\d+) for the range (\d+)\-(\d+) in row
 end
 
 When(/^I fill correct estimate (\d+) and (\d+) for the range (\d+)\- in row (\d+)$/) do |distance1, distance2, min, index|
-  within(:xpath, "//div[@class='form__fields--horizontal'][#{index}]") do
+  within(:xpath, "//div[@class='form__horizontal-fields'][#{index}]") do
     fill_in "Lähtönumerot", with: min
     fill_in "Etäisyys 1", with: distance1
     fill_in "Etäisyys 2", with: distance2
