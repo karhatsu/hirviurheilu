23871a02298cb712e87938a888cba9d32ca66df6
Henri Karhatsu <henri@karhatsu.com>
2020-01-05 12:42:32 +0200

    Rename dropdown menu css classes

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -42,13 +42,13 @@ module MenuHelper
   end
 
   def dropdown_menu_single(item)
-    raw("<div class='menu__dropdown'><div class='dropdown_menu_item'>#{item}</div></div>")
+    raw("<div class='dropdown-menu'><div class='dropdown-menu__item'>#{item}</div></div>")
   end
 
   def races_dropdown_menu(races)
-    menu = '<div class="menu__dropdown">'
+    menu = '<div class="dropdown-menu">'
     races.each do |race|
-      menu << "<div class='dropdown_menu_item'>#{link_to race.name, race_path(race)}</div>"
+      menu << "<div class='dropdown-menu__item'>#{link_to race.name, race_path(race)}</div>"
     end
     menu << "<div>#{link_to "- #{t('home.show.all_competitions')} -", races_path}</div>"
     menu << '</div>'
@@ -57,7 +57,7 @@ module MenuHelper
 
   def series_dropdown_menu(race, type)
     return '' if race.series.count <= 1
-    menu = '<div class="menu__dropdown">'
+    menu = '<div class="dropdown-menu">'
     race.series.each do |series|
       next if series.new_record?
       if type == 'results'
@@ -73,7 +73,7 @@ module MenuHelper
       elsif type == 'shots'
         link = official_series_shots_path(locale_for_path, series)
       end
-      menu << "<div class='dropdown_menu_item'>#{link_to series.name, link}</div>"
+      menu << "<div class='dropdown-menu__item'>#{link_to series.name, link}</div>"
     end
     menu << '</div>'
     raw(menu)
@@ -81,9 +81,9 @@ module MenuHelper
 
   def relays_dropdown_menu(race)
     return '' if race.relays.count <= 1
-    menu = '<div class="menu__dropdown">'
+    menu = '<div class="dropdown-menu">'
     race.relays.each do |relay|
-      menu << "<div class='dropdown_menu_item'>#{link_to relay.name, race_relay_path(locale_for_path, race, relay)}</div>"
+      menu << "<div class='dropdown-menu__item'>#{link_to relay.name, race_relay_path(locale_for_path, race, relay)}</div>"
     end
     menu << '</div>'
     raw(menu)
@@ -91,9 +91,9 @@ module MenuHelper
 
   def team_competitions_dropdown_menu(race)
     return '' if race.team_competitions.count <= 1
-    menu = '<div class="menu__dropdown">'
+    menu = '<div class="dropdown-menu">'
     race.team_competitions.each do |tc|
-      menu << "<div class='dropdown_menu_item'>#{link_to tc.name, race_team_competition_path(locale_for_path, race, tc)}</div>"
+      menu << "<div class='dropdown-menu__item'>#{link_to tc.name, race_team_competition_path(locale_for_path, race, tc)}</div>"
     end
     menu << '</div>'
     raw(menu)
@@ -101,9 +101,9 @@ module MenuHelper
 
   def cup_series_dropdown_menu(cup)
     return '' if cup.cup_series.length <= 1
-    menu = '<div class="menu__dropdown">'
+    menu = '<div class="dropdown-menu">'
     cup.cup_series.each do |cs|
-      menu << "<div class='dropdown_menu_item'>#{link_to cs.name, cup_cup_series_path(locale_for_path, cup, cs)}</div>"
+      menu << "<div class='dropdown-menu__item'>#{link_to cs.name, cup_cup_series_path(locale_for_path, cup, cs)}</div>"
     end
     menu << '</div>'
     raw(menu)
