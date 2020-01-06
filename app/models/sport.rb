4b4d12a98e78ab494ac0292650422bf4aab1d2a0
Henri Karhatsu <henri@karhatsu.com>
2020-01-06 18:00:40 +0200

    Add AIR sport key

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,15 +1,20 @@
 class Sport
   SKI = "SKI"
   RUN = "RUN"
+  AIR = "AIR"
 
   def self.by_key(key)
-    if key === RUN
+    if key == RUN
       OpenStruct.new({
           name: 'Hirvenjuoksu'
       })
-    elsif key === SKI
+    elsif key == SKI
       OpenStruct.new({
           name: 'Hirvenhiihto'
+      })
+    elsif key == AIR
+      OpenStruct.new({
+          name: 'Ilma-aseet'
       })
     else
       raise "Unknown sport key: #{key}"
