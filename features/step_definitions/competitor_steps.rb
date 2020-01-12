f315ada4b0930fd5a0fecb596773542dad753287
Henri Karhatsu <henri@karhatsu.com>
2020-01-12 09:29:34 +0200

    Remove unused competitor steps

--- features/step_definitions/competitor_steps.rb
+++ features/step_definitions/competitor_steps.rb
@@ -19,18 +19,6 @@ Given /^the series has a competitor "([^"]*)" "([^"]*)" with (\d+)\+(\d+)\+(\d+)
   @competitor.shooting_points.should == spoints.to_i
 end
 
-Given /^the series has (\d+) competitors$/ do |amount|
-  amount.to_i.times do
-    create(:competitor, :series => @series)
-  end
-end
-
-# This is against the principles of integration tests but
-# big count slows down the tests too much.
-Given /^the database contains in total (\d+) competitors$/ do |count|
-  Competitor.stub(:count).and_return(count.to_i)
-end
-
 Given /^the series has a competitor with attributes:$/ do |fields|
   hash = fields.rows_hash
   if hash[:club]
@@ -67,22 +55,6 @@ Given /^the competitor "([^"]*)" "([^"]*)" has the following results:$/ do |firs
     last_name, fields|
   competitor = Competitor.where(['first_name=? and last_name=?', first_name, last_name]).first
   competitor.attributes = fields.rows_hash
-  competitor.save!
-end
-
-Given /^the shots for the competitor "([^"]*)" "([^"]*)" are (\d+),(\d+),(\d+),(\d+),(\d+),(\d+),(\d+),(\d+),(\d+),(\d+)$/ do |first_name,
-    last_name, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10|
-  competitor = Competitor.where(['first_name=? and last_name=?', first_name, last_name]).first
-  competitor.shot_0 = s1
-  competitor.shot_1 = s2
-  competitor.shot_2 = s3
-  competitor.shot_3 = s4
-  competitor.shot_4 = s5
-  competitor.shot_5 = s6
-  competitor.shot_6 = s7
-  competitor.shot_7 = s8
-  competitor.shot_8 = s9
-  competitor.shot_9 = s10
   competitor.save!
 end
 
