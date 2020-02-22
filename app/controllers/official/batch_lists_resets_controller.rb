24464c882f1593f3b327d7438b277a1575aa3a34
Henri Karhatsu <henri@karhatsu.com>
2020-02-22 15:18:30 +0200

    Make it possible to delete also all batches

--- app/controllers/official/batch_lists_resets_controller.rb
+++ app/controllers/official/batch_lists_resets_controller.rb
@@ -7,7 +7,10 @@ class Official::BatchListsResetsController < Official::OfficialController
   def destroy
     if @race.name == params[:confirm_text]
       flash[:success] = t('.batches_cleared')
-      @race.competitors.update_all('batch_id=null, track_place=null')
+      @race.transaction do
+        @race.competitors.update_all('batch_id=null, track_place=null')
+        @race.batches.destroy_all if params[:delete_batches]
+      end
       redirect_to official_race_batches_path(@race)
     else
       flash[:error] = t('.confirm_text_was_wrong')
