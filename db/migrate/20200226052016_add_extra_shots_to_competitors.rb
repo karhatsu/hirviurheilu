ca7e378bf67aa4f1e92b1a3836fbd2c5c35b887a
Henri Karhatsu <henri@karhatsu.com>
2020-02-26 07:47:26 +0200

    Add competitors.extra_shots

---
+++ db/migrate/20200226052016_add_extra_shots_to_competitors.rb
@@ -0,0 +1,22 @@
+class AddExtraShotsToCompetitors < ActiveRecord::Migration[6.0]
+  def up
+    add_column :competitors, :extra_shots, :jsonb
+    Race.all.where(sport_key: [Sport::ILMAHIRVI, Sport::ILMALUODIKKO]).each do |race|
+      race.competitors.each do |competitor|
+        if competitor.shots && competitor.shots.length > 20
+          competitor.extra_shots = competitor.shots[20..-1]
+          competitor.shots = competitor.shots[0...20]
+          competitor.save!
+        end
+      end
+    end
+  end
+
+  def down
+    Competitor.where('extra_shots IS NOT NULL').each do |competitor|
+      competitor.shots += competitors.extra_shots
+      competitor.save!
+    end
+    remove_column :competitors, :extra_shots
+  end
+end
