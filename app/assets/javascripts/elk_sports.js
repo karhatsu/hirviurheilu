dd98d09aaf206cb273c96f39c23aa13cabde4fed
Henri Karhatsu <henri@karhatsu.com>
2020-02-27 09:10:01 +0200

    Show sub totals in shooting result forms

--- app/assets/javascripts/elk_sports.js
+++ app/assets/javascripts/elk_sports.js
@@ -14,14 +14,14 @@ function remove_fields(link, hide_class, removal_question) {
   }
 }
 
-function calculate_shots_result(row) {
+function calculateThreeSportsShotsResult(card) {
   let result = 0;
   let error = false;
-  const total = row.find('.shots-total-input').val();
+  const total = card.find('.shots-total-input').val();
   if(total !== '') {
     result = parseInt(total, 10);
     if(result >= 0 && result <= 100) {
-      row.find('.shot').each(function() {
+      card.find('.shot').each(function() {
         if($(this).val() !== '') {
           error = true;
         }
@@ -30,21 +30,59 @@ function calculate_shots_result(row) {
       error = true;
     }
   } else {
-    row.find('.shot').each(function() {
-      if($(this).val() !== '') {
-        var shot = parseInt($(this).val(), 10);
-        if(shot >= 0 && shot <= 10) {
-          result += shot;
-        } else {
-          error = true;
-        }
-      }
-    });
+    const errorAndSum = sumOfShots(card, false, 0, 10)
+    error = errorAndSum[0]
+    result = errorAndSum[1]
   }
   if(error) {
     result = '?';
   }
-  row.find('.card__main-value').text(result);
+  card.find('.card__main-value').text(result);
+}
+
+function calculateShootingRaceShotsResult(card, elevenAsTen) {
+  let result = 0
+  let error = false
+  const qualificationTotal = card.find('.shots-total-input:eq(0)').val()
+  const finalTotal = card.find('.shots-total-input:eq(1)').val()
+  let q
+  let f
+  if (qualificationTotal !== '' || finalTotal !== '') {
+    q = parseInt(qualificationTotal)
+    f = parseInt(finalTotal) || 0
+  } else {
+    const errorAndSumQ = sumOfShots(card, elevenAsTen, 0, 10)
+    const errorAndSumF = sumOfShots(card, elevenAsTen, 10, 20)
+    error = errorAndSumQ[0] || errorAndSumF[0]
+    q = errorAndSumQ[1]
+    f = errorAndSumF[1]
+  }
+  if (q >= 0 && q <= 100 && f >= 0 && f <= 100) {
+    result = q + ' + ' + f + ' = ' + (q + f)
+  } else {
+    error = true
+  }
+  if(error) {
+    result = '?';
+  }
+  card.find('.card__main-value').text(result);
+}
+
+function sumOfShots(card, elevenAsTen, firstIndex, lastIndex) {
+  const maxValue = elevenAsTen ? 11 : 10
+  let error = false
+  let sum = 0
+  card.find('.shot').slice(firstIndex, lastIndex).each(function() {
+    if($(this).val() !== '') {
+      const shot = parseInt($(this).val())
+      if(shot >= 0 && shot <= maxValue) {
+        sum += shot === 11 ? 10 : shot
+      } else {
+        error = true
+      }
+    }
+  })
+  return [error, sum]
 }
 
 function updateInlineMessage(locator, type, text) {
