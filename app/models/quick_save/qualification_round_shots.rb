5f346a7ff97c1119a6f1bef1ba41c6f78239d191
Henri Karhatsu <henri@karhatsu.com>
2020-02-27 09:24:44 +0200

    Allow sum as input in qualification round quick save

--- app/models/quick_save/qualification_round_shots.rb
+++ app/models/quick_save/qualification_round_shots.rb
@@ -1,16 +1,21 @@
 class QuickSave::QualificationRoundShots < QuickSave::QuickSaveBase
   def initialize(race_id, string)
-    super race_id, string, /^(\+\+|)\d+(\,)[\+\*0-9]{10}$/
+    super race_id, string, /^(\+\+|)\d+(\,)[0-9][0-9]?0?$/, /^(\+\+|)\d+(\,)[\+\*0-9]{10}$/
   end
 
   private
   def set_competitor_attrs
-    final_shots = @competitor.shots ? @competitor.shots[10..-1] || [] : []
-    @competitor.shots = qualification_shots + final_shots
+    if @string.length < 12
+      @competitor.shots = nil
+      @competitor.qualification_round_shooting_score_input = result_string
+    else
+      final_shots = @competitor.shots ? @competitor.shots[10..-1] || [] : []
+      @competitor.shots = qualification_shots + final_shots
+    end
   end
 
   def competitor_has_attrs?
-    @competitor.shots
+    @competitor.shots || @competitor.qualification_round_shooting_score_input
   end
 
   def qualification_shots
