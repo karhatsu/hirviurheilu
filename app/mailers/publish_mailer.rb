f14116414266806a1baa370515f8beda0bf1b571
Henri Karhatsu <henri@karhatsu.com>
2020-03-03 08:14:20 +0200

    Remove unused PublishMailer

--- app/mailers/publish_mailer.rb
+++
@@ -1,9 +0,0 @@
-class PublishMailer < ApplicationMailer
-  def publish_mail(race, user)
-    set_locale
-    @race = race
-    @user = user
-    mail :to => ADMIN_EMAIL, :from => NOREPLY_ADDRESS,
-      :subject => "Hirviurheilu - kilpailu julkaistu (#{ProductionEnvironment.name})"
-  end
-end
--- test/mailers/previews/publish_mailer_preview.rb
+++
@@ -1,5 +0,0 @@
-class PublishMailerPreview < ActionMailer::Preview
-  def publish_race
-    PublishMailer.publish_mail Race.last, Race.last.users.first
-  end
-end
