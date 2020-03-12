85acb1a2e8661193a29eda276f83dce53f8c1595
Henri Karhatsu <henri@karhatsu.com>
2020-03-12 08:53:46 +0200

    Add user removal to admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < Admin::AdminController
   def index
     @is_admin_users = true
-    @users = User.order(Arel.sql("UPPER(last_name), UPPER(first_name)"))
+    find_users
   end
 
   def new
@@ -23,8 +23,24 @@ class Admin::UsersController < Admin::AdminController
     end
   end
 
+  def destroy
+    @user = User.find params[:id]
+    if @user.races.empty?
+      @user.destroy
+      redirect_to admin_users_path
+    else
+      flash[:error] = 'Käyttäjää ei voida poistaa, koska hänellä on kilpailuja'
+      find_users
+      render :index
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :club_name, :email, :password)
+  end
+
+  def find_users
+    @users = User.order(Arel.sql("UPPER(last_name), UPPER(first_name)"))
   end
 end
