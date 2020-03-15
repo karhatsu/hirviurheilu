86665ced748311e292399466bbe5943e0581900a
Henri Karhatsu <henri@karhatsu.com>
2020-03-15 09:38:21 +0200

    Show batch type in shooting by batches page dropdown

--- app/controllers/official/shooting_by_batches_controller.rb
+++ app/controllers/official/shooting_by_batches_controller.rb
@@ -2,7 +2,7 @@ class Official::ShootingByBatchesController < Official::OfficialController
   before_action :assign_race_by_race_id, :check_assigned_race, :set_menu
 
   def index
-    @batches = @race.batches
+    @batch_options = @race.batches.sort { |a, b| [b.type, a.number] <=> [a.type, b.number] }.map {|b| [batch_name(b), b.id]}
     @batch = @race.batches.find(params[:batch_id]) unless params[:batch_id].blank?
   end
 
@@ -10,5 +10,9 @@ class Official::ShootingByBatchesController < Official::OfficialController
 
   def set_menu
     @is_shots_by_batches = true
+  end
+
+  def batch_name(batch)
+    "#{batch.is_a?(FinalRoundBatch) ? t('attributes.final_round_batch_id') : t('attributes.qualification_round_batch_id')} #{batch.number}"
   end
 end
