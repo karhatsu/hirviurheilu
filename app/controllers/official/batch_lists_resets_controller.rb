5150291382068ec5ae2d8191a4179a5344642ccf
Henri Karhatsu <henri@karhatsu.com>
2020-03-15 14:41:30 +0200

    Implement batch reset for final round

--- app/controllers/official/batch_lists_resets_controller.rb
+++ app/controllers/official/batch_lists_resets_controller.rb
@@ -5,11 +5,13 @@ class Official::BatchListsResetsController < Official::OfficialController
   end
 
   def destroy
-    if @race.name == params[:confirm_text]
+    if params[:type].blank?
+      flash[:error] = t('.choose_type')
+      render :show
+    elsif @race.name == params[:confirm_text]
       flash[:success] = t('.batches_cleared')
       @race.transaction do
-        @race.competitors.update_all('batch_id=null, track_place=null')
-        @race.batches.destroy_all if params[:delete_batches]
+        destroy_data
       end
       redirect_to official_race_batches_path(@race)
     else
@@ -22,5 +24,15 @@ class Official::BatchListsResetsController < Official::OfficialController
 
   def set_menu_batches
     @is_batches = true
+  end
+
+  def destroy_data
+    if params[:type] == 'QualificationRoundBatch'
+      @race.competitors.update_all('qualification_round_batch_id=null, qualification_round_track_place=null')
+      @race.qualification_round_batches.destroy_all if params[:delete_batches]
+    elsif params[:type] == 'FinalRoundBatch'
+      @race.competitors.update_all('final_round_batch_id=null, final_round_track_place=null')
+      @race.final_round_batches.destroy_all if params[:delete_batches]
+    end
   end
 end
