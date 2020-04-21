11768fbc241701cee8405f3e7b33dbc92b84f4c1
Henri Karhatsu <henri@karhatsu.com>
2020-04-21 08:27:45 +0300

    Do not export number to shooting race CSV
    This way the same CSV can be easily imported to another race.

--- app/models/csv_export.rb
+++ app/models/csv_export.rb
@@ -25,8 +25,11 @@ class CsvExport
   end
 
   def row(competitor)
-    columns = [competitor.first_name, competitor.last_name, competitor.club.name, series(competitor), competitor.number]
-    columns << start_time(competitor) unless @shooting_race
+    columns = [competitor.first_name, competitor.last_name, competitor.club.name, series(competitor)]
+    unless @shooting_race
+      columns << competitor.number
+      columns << start_time(competitor)
+    end
     columns
   end
 
