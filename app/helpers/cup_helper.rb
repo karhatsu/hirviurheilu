74c6f5f5c497de6cd91e4482da196e07989f25fb
Henri Karhatsu <henri@karhatsu.com>
2020-09-09 08:35:55 +0300

    Emphasize race points that are included in cup total points

--- app/helpers/cup_helper.rb
+++ app/helpers/cup_helper.rb
@@ -11,4 +11,11 @@ module CupHelper
     return cup_series.name if cup_series.has_single_series_with_same_name?
     "#{cup_series.name} (#{cup_series.series_names.split(',').join(', ')})"
   end
+
+  def min_points_to_emphasize(race_count, top_competitions, cup_competitors, is_rifle = false)
+    return nil unless race_count > top_competitions
+    points = cup_competitors.map { |cc| is_rifle ? cc.european_rifle_score : cc.points }
+    sorted_points = points.filter {|p| !p.nil?}.sort {|a, b| b <=> a}
+    sorted_points[top_competitions - 1]
+  end
 end
