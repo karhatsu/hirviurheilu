ec43f114a6ba8f1181677502d50b0e3df3b261cd
Henri Karhatsu <henri@karhatsu.com>
2021-01-09 11:24:36 +0200

    Change development cache setup to match current rails default

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,16 +11,19 @@ Hirviurheilu::Application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp/caching-dev.txt').exist?
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
-    config.static_cache_control = "public, max-age=172800"
-    config.cache_store = :mem_cache_store
+    config.action_controller.enable_fragment_cache_logging = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+    }
   else
     config.action_controller.perform_caching = false
+
     config.cache_store = :null_store
   end
-
-  config.cache_store = :memory_store
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
