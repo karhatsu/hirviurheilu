83c5e861be4779963eee4a9bd6619b7fc0649200
Henri Karhatsu <henri@karhatsu.com>
2021-05-23 12:38:55 +0300

    Use plural correctly in React race menu

--- app/javascript/src/public/menu/DesktopSecondLevelMenu.js
+++ app/javascript/src/public/menu/DesktopSecondLevelMenu.js
@@ -73,7 +73,7 @@ export default function DesktopSecondLevelMenu() {
           {race.sport.startList && (
             <DesktopMenuItem
               path={buildSeriesStartListPath(race.id, seriesId)}
-              text={t('startList')}
+              text={t('startLists')}
               routeMatch="/:lang?/races/:raceId/series/:seriesId/start_list"
               reactLink={true}
               dropdownItems={race.series.map(s => {
@@ -86,7 +86,7 @@ export default function DesktopSecondLevelMenu() {
       {race.teamCompetitions.length > 0 && (
         <DesktopMenuItem
           path={buildTeamCompetitionsPath(race.id, race.teamCompetitions[0].id)}
-          text={t('teamCompetitions')}
+          text={t(race.teamCompetitions.length > 1 ? 'teamCompetitions' : 'teamCompetition')}
           dropdownItems={race.teamCompetitions.map(tc => {
             return { text: tc.name, path: buildTeamCompetitionsPath(race.id, tc.id) }
           })}
@@ -95,7 +95,7 @@ export default function DesktopSecondLevelMenu() {
       {race.sport.european && race.teamCompetitions.length > 0 && (
         <DesktopMenuItem
           path={buildRifleTeamCompetitionsPath(race.id, race.teamCompetitions[0].id)}
-          text={t('rifleTeamCompetitions')}
+          text={t(race.teamCompetitions.length > 1 ? 'rifleTeamCompetitions' : 'rifleTeamCompetition')}
           dropdownItems={race.teamCompetitions.map(tc => {
             return { text: tc.name, path: buildRifleTeamCompetitionsPath(race.id, tc.id) }
           })}
@@ -104,7 +104,7 @@ export default function DesktopSecondLevelMenu() {
       {race.relays.length > 0 && (
         <DesktopMenuItem
           path={buildRelayPath(race.id, race.relays[0].id)}
-          text={t('relays')}
+          text={t(race.relays.length > 1 ? 'relays' : 'relay')}
           dropdownItems={race.relays.map(r => ({ text: r.name, path: buildRelayPath(race.id, r.id) }))}
         />
       )}
