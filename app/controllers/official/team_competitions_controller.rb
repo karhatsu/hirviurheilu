2267c0379ca72e97f59fc2b6c82215354874b98b
Henri Karhatsu <henri@karhatsu.com>
2021-07-13 19:10:49 +0300

    Prevent empty extra shots column in trap team competition

--- app/controllers/official/team_competitions_controller.rb
+++ app/controllers/official/team_competitions_controller.rb
@@ -61,7 +61,9 @@ class Official::TeamCompetitionsController < Official::OfficialController
       if club_id > 0
         shots1 = convert_shots params[:extra_shots1][i]
         shots2 = convert_shots params[:extra_shots2][i]
-        @tc.extra_shots << { "club_id" => club_id, "shots1" => shots1, "shots2" => shots2 }
+        unless shots1.empty? && shots2.empty?
+          @tc.extra_shots << { "club_id" => club_id, "shots1" => shots1, "shots2" => shots2 }
+        end
       end
     end
   end
