8ebe1b49f76fa63b59a6275b769cf344b75ee652
Henri Karhatsu <henri@karhatsu.com>
2021-07-23 09:20:17 +0300

    Take finished series into account for national record

--- app/javascript/src/public/series-results/NationalRecord.js
+++ app/javascript/src/public/series-results/NationalRecord.js
@@ -6,7 +6,7 @@ export default function NationalRecord({ race, series, competitor }) {
   const { points, position } = competitor
   const { nationalRecord } = series
   if (!points || !nationalRecord || points < nationalRecord || position > 1) return null
-  const text = `SE${points === nationalRecord ? ' (sivuaa)' : ''}${race.finished ? '' : '?'}`
+  const text = `SE${points === nationalRecord ? ' (sivuaa)' : ''}${race.finished || series.finished ? '' : '?'}`
   return (
     <span className="explanation">
       {' '}<a href={nationalRecordUrl} target="_blank" rel="noreferrer">{text}</a>
