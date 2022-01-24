a4f7462cb10edb4ce224673230711243c7115b16
Henri Karhatsu <henri@karhatsu.com>
2022-01-24 08:32:03 +0200

    Fix config.cache_classes warning when running cucumber

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,11 +1,8 @@
 Hirviurheilu::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
-  # The test environment is used exclusively to run your application's
-  # test suite.  You never need to work with it otherwise.  Remember that
-  # your test database is "scratch space" for the test suite and is wiped
-  # and recreated between test runs.  Don't rely on the data there!
-  config.cache_classes = false
+  # Turn false under Spring and add config.action_view.cache_template_loading = true.
+  config.cache_classes = true
 
   # Configure static asset server for tests with Cache-Control for performance
   config.public_file_server.enabled = true
