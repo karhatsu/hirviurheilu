dfca6a8158fbdce7872762197bea832431c8a3b7
Henri Karhatsu <henri@karhatsu.com>
2022-01-29 14:17:11 +0200

    Add back button to sports info page

--- app/javascript/src/public/info/SportsInfoPage.js
+++ app/javascript/src/public/info/SportsInfoPage.js
@@ -1,8 +1,11 @@
 import React, { useCallback, useEffect } from 'react'
 import useMenu, { pages } from '../../util/useMenu'
+import useTranslation from '../../util/useTranslation'
 import Button from '../../common/Button'
+import { buildRootPath } from '../../util/routeUtil'
 
 export default function SportsInfoPage() {
+  const { t } = useTranslation()
   const { setSelectedPage } = useMenu()
   useEffect(() => {
     setSelectedPage(pages.info.sportsInfo)
@@ -19,6 +22,9 @@ export default function SportsInfoPage() {
         {buildButton('lajiesittelyt', 'Lajiesittelyt')}
         {buildButton('metsastysampumasaannot', 'Lajien säännöt')}
         {buildButton('metsastysammuntojen-sm-kilpailut-2022-2024', 'Tulevat SM-kilpailut')}
+      </div>
+      <div className="buttons buttons--nav">
+        <Button to={buildRootPath()} type="back">{t('backToHomePage')}</Button>
       </div>
     </div>
   )
