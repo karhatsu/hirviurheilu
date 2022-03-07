88d9647d48508e1e5babeae25a8018d81f6c5728
Henri Karhatsu <henri@karhatsu.com>
2022-03-07 17:33:15 +0200

    Add missing yesterday's races to official index

--- app/controllers/official/index_controller.rb
+++ app/controllers/official/index_controller.rb
@@ -2,11 +2,13 @@ class Official::IndexController < Official::OfficialController
   def show
     if current_user.admin?
       @today = Race.today.order('start_date ASC')
+      @yesterday = Race.yesterday
       @future = Race.future
       @past = Race.past.order('start_date DESC')
       @cups = Cup.all.order('created_at DESC')
     else
       @today = current_user.races.today
+      @yesterday = current_user.races.yesterday
       @future = current_user.races.future
       @past = current_user.races.past
       @cups = current_user.cups
