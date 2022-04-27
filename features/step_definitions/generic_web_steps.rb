fee8c1194974c8800eccb49010903437fa933eec
Henri Karhatsu <henri@karhatsu.com>
2022-04-27 15:26:24 +0300

    Fix flaky shooting race scenarios

--- features/step_definitions/generic_web_steps.rb
+++ features/step_definitions/generic_web_steps.rb
@@ -7,6 +7,7 @@ When(/^I press the first "(.*?)" button$/) do |button_text|
 end
 
 When('I force mobile UI') do
+  page.has_css? '.results-desktop'
   page.execute_script("window.forceMobileLayout && window.forceMobileLayout(true)")
   page.execute_script("$('.results--desktop').hide()")
   page.execute_script("$('.results--mobile').show()")
