25bc8ef93a22236ed91e78a7a7b86f9101079178
Henri Karhatsu <henri@karhatsu.com>
2022-04-27 14:26:05 +0300

    Improve official index race sorting

--- app/controllers/official/index_controller.rb
+++ app/controllers/official/index_controller.rb
@@ -1,15 +1,15 @@
 class Official::IndexController < Official::OfficialController
   def show
     if current_user.admin?
-      @today = Race.today.order('start_date ASC')
-      @yesterday = Race.yesterday
+      @today = Race.today.except(:order).order('name')
+      @yesterday = Race.yesterday.except(:order).order('name')
       @future = Race.future
       @past = Race.past.order('start_date DESC')
       @cups = Cup.all.order('created_at DESC')
     else
-      @today = current_user.races.today
-      @yesterday = current_user.races.yesterday
-      @future = current_user.races.future
+      @today = current_user.races.today.except(:order).order('name')
+      @yesterday = current_user.races.yesterday.except(:order).order('name')
+      @future = current_user.races.future.except(:order).order('start_date, name')
       @past = current_user.races.past
       @cups = current_user.cups.order('created_at DESC')
     end
