77dc961afa7a982db686984e2de1b07b4f6bcdf9
Henri Karhatsu <henri@karhatsu.com>
2022-09-12 13:24:59 +0300

    Redirect to correct page if nordic sub sport series mismatch

--- app/controllers/nordic_races_controller.rb
+++ app/controllers/nordic_races_controller.rb
@@ -24,15 +24,28 @@ class NordicRacesController < ApplicationController
   private
 
   def render_page
+    return unless verify_series
     use_react
     respond_to do |format|
       format.html { render layout: true, html: '' }
       format.pdf {
-        @series = @race.series.where(id: params[:series_id]).first if params[:series_id]
         render template: 'nordic_races/show', pdf: "#{@race.name}-#{@sub_sport}-tulokset", layout: true, margin: pdf_margin,
                header: pdf_header("#{@race.name} - #{I18n.t("sport_name.nordic_sub.#{@sub_sport}")}\n"), footer: pdf_footer,
                orientation: 'Portrait', disable_smart_shrinking: true
       }
     end
+  end
+
+  def verify_series
+    if @race.nordic_sub_results_for_series? && !params[:series_id]
+      redirect_to "/races/#{@race.id}/series/#{@race.series.first.id}/#{@sub_sport}"
+      return false
+    elsif !@race.nordic_sub_results_for_series? && params[:series_id]
+      redirect_to "/races/#{@race.id}/#{@sub_sport}"
+      return false
+    elsif @race.nordic_sub_results_for_series? && params[:series_id]
+      @series = @race.series.where(id: params[:series_id]).first
+    end
+    true
   end
 end
