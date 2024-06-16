d19f8bf0284a8e225a68a57b2025df416767d5d2
Henri Karhatsu <henri@karhatsu.com>
2024-06-16 10:18:43 +0300

    Show shooting rules penalty in shooting race results

--- app/javascript/src/public/series-results/TotalScore.js
+++ app/javascript/src/public/series-results/TotalScore.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import NoResultReason from '../NoResultReason'
 
-export default function TotalScore({ noResultReason, totalScore }) {
+export default function TotalScore({ noResultReason, totalScore, penalty }) {
   if (noResultReason) {
     return <NoResultReason noResultReason={noResultReason} type="competitor" />
   }
-  return totalScore
+  return penalty ? `${totalScore} (${totalScore + penalty}-${penalty})` : totalScore
 }
