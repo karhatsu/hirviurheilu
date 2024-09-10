5fd0c264c7585aaee74380ee3594c1a846c5c468
Henri Karhatsu <henri@karhatsu.com>
2024-09-10 06:22:31 +0300

    Fix possible 500 in CupSeries public API

--- app/controllers/api/v2/public/cup_series_controller.rb
+++ app/controllers/api/v2/public/cup_series_controller.rb
@@ -1,7 +1,7 @@
 class Api::V2::Public::CupSeriesController < Api::V2::ApiBaseController
   def show
     @cup = Cup.where(id: params[:cup_id]).includes([:cup_series, races: [series: [competitors: [:age_group, :club, :series]]]]).first
-    @cup_series = CupSeries.where(id: params[:id]).first
+    @cup_series = @cup.cup_series.where(id: params[:id]).first if @cup
     render status: 404, body: nil unless @cup_series
   end
 end
