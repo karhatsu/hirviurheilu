dc246ee3850780c7f0a85b0a8e726abc2dd295b9
Henri Karhatsu <henri@karhatsu.com>
2025-05-07 09:51:18 +0300

    Run linter only when needed

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -e
-npm run lint
 if [ "$(cat STABLE_COMMIT)" != "$(git rev-list --max-count=1 HEAD)" ]
 then
   echo "Running all tests..."
   if [ "$(cat STABLE_COMMIT_RSPEC)" != "$(git rev-list --max-count=1 HEAD)" ]
   then
+    echo "Running linter..."
+    npm run lint
     echo "Running RSpec..."
     rspec spec
     git rev-list --max-count=1 HEAD > STABLE_COMMIT_RSPEC
