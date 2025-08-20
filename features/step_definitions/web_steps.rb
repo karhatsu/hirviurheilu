3e3f3ddba3f87f36acc45f86d99ea5f218020e24
Henri Karhatsu <henri@karhatsu.com>
2025-08-20 13:56:23 +0300

    Modernize the Cucumber 'should be on the page' step
    Also remove an unused legacy step.

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -217,25 +217,7 @@ Then /^the "([^"]*)" checkbox(?: within (.*))? should not be checked$/ do |label
 end
 
 Then /^(?:|I )should be on (.+)$/ do |page_name|
-  current_path = URI.parse(current_url).path
-  if current_path.respond_to? :should
-    current_path.should == path_to(page_name)
-  else
-    assert_equal path_to(page_name), current_path
-  end
-end
-
-Then /^(?:|I )should have the following query string:$/ do |expected_pairs|
-  query = URI.parse(current_url).query
-  actual_params = query ? CGI.parse(query) : {}
-  expected_params = {}
-  expected_pairs.rows_hash.each_pair{|k,v| expected_params[k] = v.split(',')}
-
-  if actual_params.respond_to? :should
-    actual_params.should == expected_params
-  else
-    assert_equal expected_params, actual_params
-  end
+  expect(page).to have_current_path(path_to(page_name), ignore_query: true)
 end
 
 Then /^show me the page$/ do
