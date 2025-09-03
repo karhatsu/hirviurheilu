a1b4aded73061492b40cf62af97256ccc53e2f20
Henri Karhatsu <henri@karhatsu.com>
2025-09-03 06:18:00 +0300

    Rename default branch

--- deploy_to_production.sh
+++ deploy_to_production.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 ./run_all_tests.sh
-git push production master
+git push production main
 heroku run rake db:migrate --app hirviurheilu
 heroku restart --app hirviurheilu
 URL=https://www.hirviurheilu.com bundle exec cucumber test/smoke/smoke.feature
--- deploy_to_staging.sh
+++ deploy_to_staging.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 ./run_all_tests.sh
-git push staging master
+git push staging main
 heroku run rake db:migrate --app hirviurheilu-testi
 heroku restart --app hirviurheilu-testi
 URL=https://testi.hirviurheilu.com bundle exec cucumber test/smoke/smoke.feature
