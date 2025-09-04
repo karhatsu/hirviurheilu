4d839278bd85273dd8ddf0486a6ab91b2804dd08
Henri Karhatsu <henri@karhatsu.com>
2025-09-04 12:26:11 +0300

    Return 404 when rifle team competition results requested for non-european race

--- app/controllers/rifle_team_competitions_controller.rb
+++ app/controllers/rifle_team_competitions_controller.rb
@@ -2,6 +2,10 @@ class RifleTeamCompetitionsController < TeamCompetitionsController
   before_action :set_races, :assign_race_by_race_id, :assign_team_competition_by_id
 
   def show
+    unless @tc.sport.european?
+      @id = @tc.id
+      return render 'errors/team_competition_not_found', status: 404
+    end
     @rifle_team = true
     @results = @tc.rifle_results
     render_page
